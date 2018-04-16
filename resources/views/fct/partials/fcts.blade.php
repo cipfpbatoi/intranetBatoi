ba926c967654b9f7b50106aa00fbe75aa7c8333b
igomis <ignasi.gomis.mullor@gmail.com>
2018-04-16 16:16:56 +0200

    fct

--- resources/views/fct/partials/fcts.blade.php
+++ resources/views/fct/partials/fcts.blade.php
@@ -2,13 +2,14 @@
 @foreach(\Intranet\Entities\FCT::where('idAlumno',$fct->idAlumno)->get() as $fct)
         <li>
             <div class="message_date">
-                <h4 class="text-info"><i class="fa fa-calendar-times-o user-profile-icon"></i> 
+                <h4 class="text-info">
                     <a href='/fct/{{$fct->id}}/show'>{{$fct->Colaboracion->Centro->nombre}}</a>
                 </h4>
                 
             </div>
             <div class="message_wrapper">
-                    <h4 class="text-info">{{$fct->desde}} - {{$fct->hasta}}</h4>
+                    <h4 class="text-info"><i class="fa fa-calendar-times-o user-profile-icon"></i>{{$fct->desde}} - {{$fct->hasta}}</h4>
+                    <h4 class="text-info">{{$fct->horas}} {{trans('messages.generic.horas')}}</h4>
             </div>
         </li>    
 @endforeach
