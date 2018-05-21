959e25cc398c92fd208423f51ad7e9766c4816bd
igomis <ignasi.gomis.mullor@gmail.com>
2018-05-21 13:25:19 +0200

    numeracion

--- config/constants.php
+++ config/constants.php
@@ -125,7 +125,7 @@ return [
     'prioridadIncidencia' => ['Baja','Media','Alta'],
     'tipoVehiculo' => ['Avion','Tren','Taxi','Autobus','Otros'],
     'estadoDocumento' => ['Creado','Pendiente','Autorizado','Impreso'],
-    'numeracion' => ['--','01','02','03','04','05','06','07','08','09','AvaIni','1Ava','2Ava','3Ava','AvFinal','AvExtr',21=>'1er Trimestre',22=>'2on Trimestre',23=>'Final'],
+    'numeracion' => ['--','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15',30=>'AvaIni',31=>'1Ava',32=>'2Ava',33=>'3Ava',34=>'AvFinal',35=>'AvExtr',21=>'1er Trimestre',22=>'2on Trimestre',23=>'Final'],
     'nombreEval' => [1=>'Primera', 2=> 'Segona', 3=>'Final', 4=>'Extraordinària'],
     'checkModels' => ['Programacion'=>'Programaciones'],
     'tipoExpediente' => [   1=>'Baixa Inasistència',
