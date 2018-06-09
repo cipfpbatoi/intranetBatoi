c1750e0419d83eff5dbea2b8a5273c1a6c531eea
igomis <ignasi.gomis.mullor@gmail.com>
2018-06-09 12:43:55 +0200

    ordenes en blanc

--- resources/views/reunion/partials/ordenes.blade.php
+++ resources/views/reunion/partials/ordenes.blade.php
@@ -8,8 +8,8 @@
                 <tr><th style="width: 5%">@lang("validation.attributes.orden")</th><th style="width: 40%">@lang("validation.attributes.descripcion")</th> <th style="width: 40%">@lang("validation.attributes.resumen")</th><th style="width: 10%">@lang("validation.attributes.operaciones")</th></tr>
                 @foreach ($ordenes as $orden)
                 <tr class="lineaGrupo" id='{{ $orden->id }}'>
-                    <td><span class='input' name='orden'>{!! $orden->orden !!}</span></td>
-                    <td><span class='input' name='descripcion'>{!! $orden->descripcion !!}</span></td> 
+                    <td><span class='input'  name='orden'>{!! $orden->orden !!}</span></td>
+                    <td><span class='input'  name='descripcion'>{!! $orden->descripcion !!}</span></td> 
                     <td><span class='textarea' name='resumen'>{!! $orden->resumen !!}</span></td> 
                     <td><span class='botones'>
                             <a href="/reunion/{!!$elemento->id!!}/borrarOrden/{!! $orden->id !!}" class="delGrupo">{!! Html::image('img/delete.png',trans("messages.buttons.delete"),array('class' => 'iconopequeno','title'=>trans("messages.buttons.delete"))) !!}</a>
@@ -23,8 +23,8 @@
                     {{ csrf_field() }}
                     <input type='hidden' name='idReunion' value="{!!$elemento->id!!}">
                     <tr>
-                        <td><input type='text' name='orden' class="form-control"></td>
-                        <td><input type='text' name='descripcion' class="form-control"></td>
+                        <td><input type='text' required name='orden' class="form-control"></td>
+                        <td><input type='text' required name='descripcion' class="form-control"></td>
                         <td><textarea  rows="1"  name='resumen'class="form-control" ></textarea></td>
                         <td><input id="submit" class="boton" type="submit" value="@lang("messages.generic.anadir") @lang("models.modelos.OrdenReunion") "></td>
                     </tr>
