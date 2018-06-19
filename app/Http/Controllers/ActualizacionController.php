15d5aa35027e0dc7acba0f414966e2f529466fbd
igomis <ignasi.gomis.mullor@gmail.com>
2018-06-19 21:11:57 +0200

    versiones

--- app/Http/Controllers/ActualizacionController.php
+++ app/Http/Controllers/ActualizacionController.php
@@ -5,27 +5,20 @@
 namespace Intranet\Http\Controllers;
 
 use Intranet\Http\Controllers\Controller;
-use Intranet\Botones\Panel;
 use Illuminate\Support\Facades\Session;
-use Intranet\Entities\Programacion;
-use Intranet\Entities\Modulo_ciclo;
-use Intranet\Entities\Modulo_grupo;
-use Intranet\Entities\Ciclo;
-use Intranet\Entities\Resultado;
-use DB;
-use Intranet\Entities\Horario;
-use Intranet\Entities\Grupo;
 use Styde\Html\Facades\Alert;
-use Intranet\Entities\Profesor;
 use Illuminate\Support\Facades\Storage;
+use Intranet\Http\Controllers\AdministracionController;
 
 class ActualizacionController extends Controller{
     
   protected function actualizacion(){
-        system('git pull');
-        system('php ./../artisan config:cache');
-        system('php ./../artisan migrate');
-        $version_nueva = end(config('constants.version'));
+        system('rm ./../app/Http/Controllers/AdministracionController.php');
+        Alert::info(system('git pull'));
+        Alert::info(system('php ./../artisan config:cache'));
+        Alert::info(system('php ./../artisan migrate'));
+        $versiones = config('constants.version');
+        $version_nueva = end($versiones);
         if (Storage::exists('version.txt')) $version_actual = Storage::get('version.txt');
         else $version_actual = 'v_0';
         if ($version_nueva > $version_actual){
