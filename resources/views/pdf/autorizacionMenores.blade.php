56255921946c6e2046b1c32957999146b53057b8
igomis <ignasi.gomis.mullor@gmail.com>
2018-07-09 07:53:34 +0200

    img

--- resources/views/pdf/autorizacionMenores.blade.php
+++ resources/views/pdf/autorizacionMenores.blade.php
@@ -2,20 +2,7 @@
 @section('content')
     @foreach ($todos as $elemento)
     <div class='page'>
-        <div class="container col-lg-12" style="margin-bottom: 30px;" >
-            <div style="float:left;width:30%">
-                <img src="{{url('img/pdf/Conselleria-B_CON.jpg')}}" width="140px" height="70px"/><br/>
-                <span style='font-size: xx-small'>
-                {!! config('contacto.direccion') !!}<br/>    
-                Tel - {!! config('contacto.telefono') !!} - Fax: {!! config('contacto.fax') !!}<br/>
-                e-mail: {!! config('contacto.email') !!} <br/>
-                {!! config('contacto.web') !!} <br/></span>
-            </div>
-            <div style="float:left;width:30%">
-                <img src="{{url('img/pdf/cipfpbatoi.jpg')}} " width="180px" height="60px"/><br/>
-            </div>
-            <div style="float:left;width:30%;padding-left: 150px"><img src="{{url('img/pdf/FSEpos_val.jpg')}}" width="90px" height="90px"/></div>
-        </div>
+        @include('pdf.partials.cabecera')
         <div class="container col-lg-12" >
             <p><strong>Asistencia a actividad extraescolar. Asistència a activitat extraescolar.</strong></p>
             <p>D/Dña __________________________________________________________ padre/madre o tutor/a de             
