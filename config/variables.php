2581fe9e541942caae62bc4ddd04a666437fc119
igomis <ignasi.gomis.mullor@gmail.com>
2018-09-26 11:43:28 +0200

    expediente

--- config/variables.php
+++ config/variables.php
@@ -5,6 +5,6 @@ return [
     'diasNoCompleta' => 45,
     'precioKilometro' => 0.19,
     'reservaAforo' => 1.2,
-    'programaciones' => [ 'fichero' => '1' , 'enlace' => '1'],
-    'mostrarProgramacionesEstado' => '3',
+    'programaciones' => [ 'fichero' => '0' , 'enlace' => '1'],
+    'mostrarProgramacionesEstado' => '4',
 ];
