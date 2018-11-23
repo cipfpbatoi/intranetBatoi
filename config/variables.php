4fc121ad1ae24ae3d3e49de473a166578e414e12
igomis <ignasi.gomis.mullor@gmail.com>
2018-11-23 13:41:53 +0100

    pg0301

--- config/variables.php
+++ config/variables.php
@@ -7,5 +7,5 @@ return [
     'reservaAforo' => 1.2,
     'programaciones' => [ 'fichero' => '0' , 'enlace' => '1'],
     'mostrarProgramacionesEstado' => '3',
-    'ipGuardias' => ['172.16.20.238','172.16.20.227'],
+    'ipGuardias' => ['172.16.20.238'],
 ];
