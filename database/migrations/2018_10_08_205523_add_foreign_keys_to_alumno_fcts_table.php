7536fbf5b431b329344d2aaa7212976fbcd9078e
igomis <ignasi.gomis.mullor@gmail.com>
2018-12-17 11:50:20 +0100

    quitar integridad referencial

--- database/migrations/2018_10_08_205523_add_foreign_keys_to_alumno_fcts_table.php
+++ database/migrations/2018_10_08_205523_add_foreign_keys_to_alumno_fcts_table.php
@@ -15,7 +15,7 @@ class AddForeignKeysToAlumnoFctsTable extends Migration {
 		Schema::table('alumno_fcts', function(Blueprint $table)
 		{
 			//$table->foreign('idFct')->references('id')->on('fcts')->onUpdate('CASCADE')->onDelete('CASCADE');
-			$table->foreign('idAlumno')->references('nia')->on('alumnos')->onUpdate('CASCADE')->onDelete('RESTRICT');
+			//$table->foreign('idAlumno')->references('nia')->on('alumnos')->onUpdate('CASCADE')->onDelete('RESTRICT');
 		});
 	}
 
