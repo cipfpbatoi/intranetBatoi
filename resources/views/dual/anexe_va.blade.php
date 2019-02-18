636d2123c2d753c32f54a488d95036aead6f9540
igomis <ignasi.gomis.mullor@gmail.com>
2019-02-18 10:05:03 +0100

    dni instructor

--- resources/views/dual/anexe_va.blade.php
+++ resources/views/dual/anexe_va.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.dual')
 @section('content')
         @include('dual.partials.anexe_va',['imagen'=>'img/pdf/dual/anexe_va_i.jpg','top'=>0])
-        @include('dual.partials.anexe_va',['imagen'=>'img/pdf/dual/anexe_va_ii.jpg','top'=>1040])
+        @include('dual.partials.anexe_va',['imagen'=>'img/pdf/dual/anexe_va_ii.jpg','top'=>1000])
 @endsection
--- resources/views/dual/anexe_vb.blade.php
+++ resources/views/dual/anexe_vb.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.dual')
 @section('content')
         @include('dual.partials.anexe_vb',['imagen'=>'img/pdf/dual/anexe_vb_i.jpg','top'=>0])
-        @include('dual.partials.anexe_vb',['imagen'=>'img/pdf/dual/anexe_vb_ii.jpg','top'=>1140])
+        @include('dual.partials.anexe_vb',['imagen'=>'img/pdf/dual/anexe_vb_ii.jpg','top'=>1150])
 @endsection
