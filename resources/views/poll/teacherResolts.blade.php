8a7feb7d1b19574ec5f00dabac3b5ec542dc9614
igomis <ignasi.gomis.mullor@gmail.com>
2019-03-08 10:40:11 +0100

    poll

--- resources/views/poll/teacherResolts.blade.php
+++ resources/views/poll/teacherResolts.blade.php
@@ -6,39 +6,41 @@
     <!-- page content -->
     <div class="x_content">
         @foreach ($myVotes as $modulo => $moduloVotes)
-            @php
-                $codigo =$moduloVotes->first()->ModuloGrupo->Grupo->codigo;
-                $allByOptions = $myGroupsVotes[$codigo]->groupBy('option_id');
-            @endphp
-            <h2>{{$modulo}} del grup {{$codigo}}</h2>
-            <table style="border: #00aeef 1px solid">
-                <thead>
-                <tr>
-                    <td>Enquesta</td>
-                    @foreach ($options_numeric as $item) <th>{{$item->question}} </th> @endforeach
-                </tr>
-                </thead>
-                <tr>
-                    <td>Jo</td>
-                    @foreach ($moduloVotes->groupBy('option_id') as $option)
-                        @if ($option->sum('value')>0)
-                            <td> {{round($option->avg('value'),1)}}</td>
-                        @endif
-                    @endforeach
-                </tr>
-                <tr>
-                    <td>Grup</td>
-                    @foreach ($allByOptions as $option)
-                        @if ($option->sum('value')>0)
-                            <td>{{round($option->avg('value'),1)}} </td>
-                        @endif
-                    @endforeach
-                </tr>
-            </table>
-            <h2>Comentaris:</h2>
-            @foreach ($moduloVotes->whereIn('option_id',hazArray($options_text,'id')) as $votes)
-                <p>{{$votes->text}}</p>
-            @endforeach
+            @if ($moduloVotes->first())
+                @php
+                    $codigo =$moduloVotes->first()->ModuloGrupo->Grupo->codigo;
+                    $allByOptions = $myGroupsVotes[$codigo]->groupBy('option_id');
+                @endphp
+                <h2>{{$modulo}} del grup {{$codigo}}</h2>
+                <table style="border: #00aeef 1px solid">
+                    <thead>
+                    <tr>
+                        <td>Enquesta</td>
+                        @foreach ($options_numeric as $item) <th>{{$item->question}} </th> @endforeach
+                    </tr>
+                    </thead>
+                    <tr>
+                        <td>Jo</td>
+                        @foreach ($moduloVotes->groupBy('option_id') as $option)
+                            @if ($option->sum('value')>0)
+                                <td> {{round($option->avg('value'),1)}}</td>
+                            @endif
+                        @endforeach
+                    </tr>
+                    <tr>
+                        <td>Grup</td>
+                        @foreach ($allByOptions as $option)
+                            @if ($option->sum('value')>0)
+                                <td>{{round($option->avg('value'),1)}} </td>
+                            @endif
+                        @endforeach
+                    </tr>
+                </table>
+                <h2>Comentaris:</h2>
+                @foreach ($moduloVotes->whereIn('option_id',hazArray($options_text,'id')) as $votes)
+                    <p>{{$votes->text}}</p>
+                @endforeach
+            @endif
         @endforeach
 
     </div>
