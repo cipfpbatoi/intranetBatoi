1ee66db415b0f2ef0d411faf948f1830e1138377
igomis <ignasi.gomis.mullor@gmail.com>
2019-05-10 13:48:12 +0200

    pruebas correos

--- resources/views/colaboracion/partials/contactos.blade.php
+++ resources/views/colaboracion/partials/contactos.blade.php
@@ -4,7 +4,7 @@
             <div class="message_wrapper">
                 <h4><i class="fa fa-calendar user-profile-icon"> </i> {!! $contacto->created_at !!}
                 <h4><i class="fa fa-envelope user-profile-icon"></i>{{$contacto->comentaril}}</h4>
-                <h4 class="text-info">{{$colaboracion->Propietario->fullName}}</h4>
+                <h4 class="text-info">{{$contacto->Propietario->fullName}}</h4>
             </div>
         </li>
     @endforeach
