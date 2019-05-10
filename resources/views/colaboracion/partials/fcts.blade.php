95b00df5f6e36995cf80ff6ab34fcd5399e0d349
igomis <ignasi.gomis.mullor@gmail.com>
2019-05-10 14:04:35 +0200

    pruebas correos

--- resources/views/colaboracion/partials/fcts.blade.php
+++ resources/views/colaboracion/partials/fcts.blade.php
@@ -4,6 +4,7 @@
             <div class="message_wrapper">
                 <h5>
                     <i class="fa fa-calendar user-profile-icon"></i> {!! $contacto->created_at !!}
+                    <i class="fa fa-mortar-board user-profile-icon"></i> {{ \Intranet\Entities\Alumno::find($contacto->model_id)->fullName }}
                     <i class="fa fa-envelope user-profile-icon"></i> {{$contacto->comentari}}
                     <i class="fa fa-user user-profile-icon"></i> {{$contacto->Propietario->fullName}}
                 </h5>
