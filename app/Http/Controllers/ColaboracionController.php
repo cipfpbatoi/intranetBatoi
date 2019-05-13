824454c9d1ee18f010cdc8ed04e764c33c1af31d
igomis <ignasi.gomis.mullor@gmail.com>
2019-05-13 12:27:46 +0200

    pruebas correos

--- app/Http/Controllers/ColaboracionController.php
+++ app/Http/Controllers/ColaboracionController.php
@@ -165,6 +165,8 @@ class ColaboracionController extends IntranetController
         $fcts = Fct::where('idColaboracion',$id)->where('asociacion',1)->get();
         $contactCol->concat(Activity::where('model_class','Intranet\Entities\Fct')->where('action','email')->
         whereIn('model_id',hazArray($fcts,'id','id'))->get());
+        dd(Activity::where('model_class','Intranet\Entities\Fct')->where('action','email')->
+        whereIn('model_id',hazArray($fcts,'id','id'))->get());
         $alumnos = [];
         foreach ($fcts as $fct)
             $alumnos = array_merge($alumnos,hazArray($fct->Alumnos,'nia','nia'));
