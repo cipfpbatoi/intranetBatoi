874deabd67920a3011b44454db169be72c5134a4
igomis <ignasi.gomis.mullor@gmail.com>
2019-05-13 12:33:24 +0200

    pruebas correos

--- app/Http/Controllers/ColaboracionController.php
+++ app/Http/Controllers/ColaboracionController.php
@@ -161,12 +161,12 @@ class ColaboracionController extends IntranetController
     {
         $elemento = Colaboracion::findOrFail($id);
         $contactCol = Activity::where('model_class','Intranet\Entities\Colaboracion')->where('action','email')->
-            where('model_id',$id)->get();
+            where('model_id',$id)->get()
         $fcts = Fct::where('idColaboracion',$id)->where('asociacion',1)->get();
-        $contactCol->concat(Activity::where('model_class','Intranet\Entities\Fct')->where('action','email')->
-        whereIn('model_id',hazArray($fcts,'id','id'))->get());
-        dd(Activity::where('model_class','Intranet\Entities\Fct')->where('action','email')->
-        whereIn('model_id',hazArray($fcts,'id','id'))->get());
+        $contactFct = Activity::where('model_class','Intranet\Entities\Fct')->where('action','email')->
+            whereIn('model_id',hazArray($fcts,'id','id'))->get();
+        $contactCol->concat($contactFct);
+        dd($contactCol);
         $alumnos = [];
         foreach ($fcts as $fct)
             $alumnos = array_merge($alumnos,hazArray($fct->Alumnos,'nia','nia'));
