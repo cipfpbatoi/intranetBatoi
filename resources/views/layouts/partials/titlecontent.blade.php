2fb833b668a7942bfd4fd04602edb2210795ae9c
igomis <ignasi.gomis.mullor@gmail.com>
2019-07-16 12:35:31 +0200

    Fct

--- resources/views/layouts/partials/titlecontent.blade.php
+++ resources/views/layouts/partials/titlecontent.blade.php
@@ -15,7 +15,7 @@
         </ul>
     </li>
     @if ($ajuda = exists_help(substr(url()->current(), strlen(url('/')))))
-        <li><a href="/help/{{$ajuda}}"><i class="fa fa-question"></i></a>
+        <li><a href="/help/{{$ajuda}}" target="_blank"><i class="fa fa-question"></i></a>
     @endif
     <li><a class="close-link"><i class="fa fa-close"></i></a>
     </li>
