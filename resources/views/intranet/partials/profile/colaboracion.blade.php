96462fd2cdd89aea5fdf14313553aea8cc1057aa
igomis <ignasi.gomis.mullor@gmail.com>
2019-09-19 13:40:36 +0200

    fcts

--- resources/views/intranet/partials/profile/colaboracion.blade.php
+++ resources/views/intranet/partials/profile/colaboracion.blade.php
@@ -1,4 +1,8 @@
-@foreach ($panel->getElementos($pestana) as $elemento)
+@php
+    $misElementos = $panel->getElementos($pestana)->where('tutor',AuthUser()->dni);
+    $otros =  $panel->getElementos($pestana)->where('tutor','!=',AuthUser()->dni);
+@endphp
+@foreach ($misElementos as $elemento)
     @php
         $fcts = \Intranet\Entities\Fct::where('idColaboracion',$elemento->id)->where('asociacion',1)->get();
     @endphp
@@ -13,6 +17,26 @@
     @else
         @php
         $contactos = \Intranet\Entities\Activity::mail('Colaboracion')->id($elemento->id)->orderBy('created_at')->get();
+        @endphp
+        @include ('intranet.partials.profile.partials.colaboracion')
+    @endif
+@endforeach
+<hr/>
+@foreach ($otros as $elemento)
+    @php
+        $fcts = \Intranet\Entities\Fct::where('idColaboracion',$elemento->id)->where('asociacion',1)->get();
+    @endphp
+    @if (count($fcts))
+        @foreach ($fcts as $fct)
+            @php
+                $contactos = \Intranet\Entities\Activity::mail('Fct')->id($fct->id)->orderBy('created_at')->get();
+                $alumnos = $fct->Alumnos;
+            @endphp
+            @include('intranet.partials.profile.partials.fct')
+        @endforeach
+    @else
+        @php
+            $contactos = \Intranet\Entities\Activity::mail('Colaboracion')->id($elemento->id)->orderBy('created_at')->get();
         @endphp
         @include ('intranet.partials.profile.partials.colaboracion')
     @endif
