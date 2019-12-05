88692b7270103f2061b67d3fce1d4522a81be4a3
igomis <ignasi.gomis.mullor@gmail.com>
2019-12-05 13:02:31 +0100

    images

---
+++ database/migrations/2019_12_08_205523_add_foreign_keys_to_modulo_grupos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddForeignKeysToModuloGruposTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('modulo_grupos', function(Blueprint $table)
+		{
+            $table->string('idGrupo',5)->collation('utf8_unicode_ci')->change();
+			$table->foreign('idModuloCiclo')->references('id')->on('modulo_ciclos')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('idGrupo')->references('codigo')->on('grupos')->onUpdate('CASCADE')->onDelete('CASCADE');
+
+        });
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('modulo_grupos', function(Blueprint $table)
+		{
+			$table->dropForeign('modulo_grupos_idgrupo_foreign');
+            $table->dropForeign('modulo_grupos_idmodulociclo_foreign');
+
+        });
+	}
+
+}
