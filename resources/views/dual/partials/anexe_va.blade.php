740d9516ad6f5ff52032f77b1ec8527364875c06
Ignasi Gomis Mullor <igomis@cipfpbatoi.es>
2020-07-10 00:33:01 +0200

    pdf

--- resources/views/dual/partials/anexe_va.blade.php
+++ resources/views/dual/partials/anexe_va.blade.php
@@ -1,5 +1,5 @@
 <div style="position:absolute;left:50%;margin-left:-691px;top:{{$top}}px;width:1383px;height:962px;border-style:outset;overflow:visible">
-    <div style="position:absolute;left:0px;top:0px"><img src="{{url($imagen)}}" width=1383 height=962></div>
+    <div style="position:absolute;left:0px;top:0px"><img src="{{public_path($imagen)}}" width=1383 height=962></div>
     <div style="position:absolute;left:273.20px;top:162.70px;width: 400px" >
         <span>{{$datosInforme['secretario']}}</span>
     </div>
