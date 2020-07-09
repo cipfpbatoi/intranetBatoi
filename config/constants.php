559f368bd565cc54e440b97de990eb1ab8afb3ea
Ignasi Gomis Mullor <igomis@cipfpbatoi.es>
2020-07-09 23:45:02 +0200

    pdf

--- config/constants.php
+++ config/constants.php
@@ -6,7 +6,7 @@ return [
     'version' => ['v1_0','v1_1','v1_2','v1_3_4','v2_0','v2_01'],
     'modulosNoLectivos' => ['TU01CF','TU02CF'],
     'modulosSinProgramacion' => ['TUT01CF'],
-    'pdf' => 'DomPdf',
-    //'pdf' => 'SnappyPdf',
+    //'pdf' => 'DomPdf',
+    'pdf' => 'SnappyPdf',
 ];
 
