aec87cf7b05b6004ea60fdec97503ef6e4ee1569
igomis <igomis@cipfpbatoi.es>
2020-09-07 15:17:31 +0200

    curso

--- config/curso.php
+++ config/curso.php
@@ -2,9 +2,9 @@
 
 return [
     'evaluaciones' => [
-        1 => ['2019-09-01','2019-11-28'],
-        2 => ['2019-11-29','2020-02-27'],
-        3 => ['2020-02-28','2020-06-17']
+        1 => ['2020-09-01','2020-11-28'],
+        2 => ['2021-11-29','2021-02-27'],
+        3 => ['2021-02-28','2021-06-17']
     ],
     'trimestres' => [
         1 => [ 1=> 1, 2=>1],
@@ -12,12 +12,10 @@ return [
         3 => [ 1=> 3, 2=>3]
     ],
     'fct' => [
-        1 => ['inici' => '2019-09-01','fi' => '2020-01-31'],
-        2 => ['inici' =>'2020-03-04','fi' =>'2020-07-31']
+        1 => ['inici' => '2020-09-01','fi' => '2021-01-31'],
+        2 => ['inici' =>'2021-03-04','fi' =>'2021-07-31']
     ],
-    'certificatFol' => '2020-06-14',
+    'certificatFol' => '2021-06-14',
     'enquestesAutomatiques' => 0,
     'convocatoria' => 'My0yMDIwMDcwOTAwLTIwMjAwOTA5MjM'
-    //'convocatoria' => 'My0yMDIwMDcwOS0yMDIwMDgwMQ'
-    //'convocatoria' => 'NC0yMDIwMDcxNy0yMDIwMDcyMw'
 ];
