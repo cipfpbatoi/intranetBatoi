14469c5aba1ead6a6a5da37838ac4e4b957feec3
igomis <igomis@cipfpbatoi.es>
2020-10-02 11:27:34 +0200

    exempt

--- resources/views/pdf/alumnos/fse.blade.php
+++ resources/views/pdf/alumnos/fse.blade.php
@@ -5,26 +5,26 @@
 @else
     @include('pdf.partials.cabeceraBasica')
 @endif
-<div class="container col-lg-12" >
+<div class="container" >
     <p><strong>COMUNICACIÓ A l'ALUMNAT SOBRE EL COFINANÇAMENT PEL FONS SOCIAL
         EUROPEU DE LA FORMACIÓ PROFESSIONAL GM I GS I BÀSICA 2ª OPORTUNITAT
         EN LA COMUNITAT VALENCIANA</strong></p>
     <table class="table table-bordered">
         <tr><th colspan="3">Centre Educatiu</th></tr>
         <tr><th>Codi</th><th>Denominació</th><th>Municipi</th></tr>
-        <tr><td><span style="font-size: 12px">{{ config('contacto.codi') }}</span></td><td><span style="font-size: 12px">{{ config('contacto.nombre') }}</span></td><td><span style="font-size: 12px">{{ config('contacto.poblacion') }}</span></td></tr>
+        <tr><td><span style="font-size: 16px">{{ config('contacto.codi') }}</span></td><td><span style="font-size: 16px">{{ config('contacto.nombre') }}</span></td><td><span style="font-size: 16px">{{ config('contacto.poblacion') }}</span></td></tr>
     </table>   
     <table class="table table-bordered" >
-        <tr><td style="text-align: left"><strong>Nom del cicle: </strong><span style="font-size: 12px">{{$datosInforme->Ciclo->ciclo}}</span></td><td style="text-align: left; font-weight: bold">Grau:</td></tr>
-        <tr><td style="text-align: left"><strong>Curs/Grup: </strong><span style="font-size: 12px">{{$datosInforme->nombre}}</span></td><td style="text-align: left; font-weight: bold">Data:</td></tr>
-        <tr><td style="text-align: left"><strong>Tutor/a: </strong><span style="font-size: 12px">{{$datosInforme->Tutor->FullName}}</span></td><td style="text-align: left; font-weight: bold">Signatura:<br/><br/><br/><br/></td></tr>
+        <tr><td style="text-align: left"><strong>Nom del cicle: </strong><span style="font-size: 16px">{{$datosInforme->Ciclo->ciclo}}</span></td><td style="text-align: left; font-weight: bold">Grau:</td></tr>
+        <tr><td style="text-align: left"><strong>Curs/Grup: </strong><span style="font-size: 16px">{{$datosInforme->nombre}}</span></td><td style="text-align: left; font-weight: bold">Data:</td></tr>
+        <tr><td style="text-align: left"><strong>Tutor/a: </strong><span style="font-size: 16px">{{$datosInforme->Tutor->FullName}}</span></td><td style="text-align: left; font-weight: bold">Signatura:<br/><br/><br/><br/></td></tr>
     </table>   
     <p>S'ha facilitat la informació sobre el cofinançament del Fons Social Europeu dels cicles de FP GM i GS 
         i FP Bàsica de 2ª oportunitat a la Comunitat Valenciana a l'alumnat següent: </p>
     <table class="table table-bordered">
         <tr><th>Cognoms i Nom</th><th>Cognoms i Nom</th></tr><tr>
         @foreach ($todos as $key => $elemento)
-        <td><span style="font-size: 12px">{{$elemento->apellido1.' '.$elemento->apellido2.', '.$elemento->nombre}}</span></td>
+        <td><span style="font-size: 18px">{{$elemento->apellido1.' '.$elemento->apellido2.', '.$elemento->nombre}}</span></td>
             @if ($key % 2 == 1) <tr/><tr> @endif  
         @endforeach
         </tr>
