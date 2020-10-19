bce846d5bf3cf9e8017196aac33a0d36493fd1fc
igomis <igomis@cipfpbatoi.es>
2020-10-19 11:03:57 +0200

    Sort dual

--- app/Http/Controllers/DualAlumnoController.php
+++ app/Http/Controllers/DualAlumnoController.php
@@ -231,7 +231,7 @@ class DualAlumnoController extends FctAlumnoController
         $array['Texto28'] = '';
         $array['Texto29'] = 'Espanya';
         $array['Text30'] = $fct->Fct->Colaboracion->Centro->telefono;
-        $array['Text31'] = $array['Texto30'];
+        $array['Text31'] = $array['Text30'];
         $array['Text32'] = $fct->Fct->Instructor->Nombre;
         $array['Text33'] = $fct->Fct->Instructor->dni;
         $array['Text34'] = $fct->Fct->Instructor->email;
