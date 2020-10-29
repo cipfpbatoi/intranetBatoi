446f6f70c9f500b253d3e6003146510c4893678b
igomis <igomis@cipfpbatoi.es>
2020-10-29 13:30:58 +0100

    Dual

--- app/Http/Controllers/DualAlumnoController.php
+++ app/Http/Controllers/DualAlumnoController.php
@@ -176,7 +176,7 @@ class DualAlumnoController extends FctAlumnoController
 
     private function deleteDir($folder)
     {
-        $files = glob("$folder/*"); //obtenemos todos los nombres de los ficheros
+        $files = glob("$folder*"); //obtenemos todos los nombres de los ficheros
         foreach ($files as $file) {
             if (is_file($file)) unlink($file); //elimino el fichero
         }
