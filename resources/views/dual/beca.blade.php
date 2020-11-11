a138f07e4701c584d2112984cfc911cd2b7c4555
igomis <igomis@cipfpbatoi.es>
2020-11-11 07:31:15 +0100

    beca

--- resources/views/dual/beca.blade.php
+++ resources/views/dual/beca.blade.php
@@ -20,7 +20,7 @@
     </p>
     <p style="font-size: 20px;text-align: justify;margin-top: 75px;line-height: 1.5em">
         {{ $todos->Alumno->sexo=='H'?"L’alumne":"L'alumna" }} amb DNI <strong>{{ $todos->Alumno->dni }}</strong>, {{ $todos->Alumno->sexo=='H'?"el":"la" }} qual realitza a la nostra entitat la FP Dual, percebrà una ajuda a l'estudi consistent en
-        {{ $todos->beca }} € per hora treballada durant el peíode del projecte de formació especificat en el calendari de pràctiques.
+        {{ $todos->beca }} € per hora emprada durant el peíode del projecte de formació especificat en el calendari de pràctiques.
     </p>
     <p style="font-size: 20px;text-align: justify;margin-top: 75px;line-height: 1.5em">
         L'abonament d'aquesta ajuda es realitzarà mitjançant transferència bancària el número de compte aportat per l'alumne. De la mateixa manera ambdues parts signaran un document on es registrarà que l'alumne percep l'esmentada ajuda.
