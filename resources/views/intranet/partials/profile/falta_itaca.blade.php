dc9d01a5fd8fded74402f81f8651aadbeaaee5be
igomis <igomis@cipfpbatoi.es>
2021-03-04 10:38:11 +0100

    Dual error

--- resources/views/intranet/partials/profile/falta_itaca.blade.php
+++ resources/views/intranet/partials/profile/falta_itaca.blade.php
@@ -32,7 +32,7 @@
                                 </ul>
                             </div>
                             <div class="right col-xs-4 text-center">
-                                <img src="{{ asset($usuario->first()->Profesor->foto) }}" alt="" class="img-circle img-responsive">
+                                <img src="{{ asset('storage/'.$usuario->first()->Profesor->foto) }}" alt="" class="img-circle img-responsive">
                             </div>
                         </div>
                         <div class="col-xs-12 bottom text-center">
--- resources/views/pdf/alumnos/partials/foto.blade.php
+++ resources/views/pdf/alumnos/partials/foto.blade.php
@@ -1,4 +1,4 @@
 <div style="float:left;width:23%;margin-bottom:5px;margin-right:10px;height:160px;{{ $style??'' }}"    >
-    <img src="{{$elemento->foto?public_path($elemento->foto):''}}" style="width:125px;height:125px;margin-left:20;border:thin #000 solid;" /><br/>
+    <img src="{{$elemento->foto?asset('storage/'.$elemento->foto):''}}" style="width:125px;height:125px;margin-left:20;border:thin #000 solid;" /><br/>
     <h6 style="margin-top:-1px;clear:both;font-size:xx-small;">{{$elemento->nombre}} <b>{{  $elemento->posicion??'' }}</b> </h6>
 </div>
