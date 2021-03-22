a509de672a6ca99a2acf882040c9a8dd3a3a6f2a
igomis <igomis@cipfpbatoi.es>
2021-03-22 20:19:05 +0100

    Refactorizacion hasta modal component

--- resources/views/pdf/fct/pasqua.blade.php
+++ resources/views/pdf/fct/pasqua.blade.php
@@ -21,7 +21,7 @@
                 <ul>
                 @foreach ($todos as $alumno)
                     @if ($alumno->festiusEscolars == 0)
-                        <li>{{$alumno->fullName}}</li>
+                        <li>{{$alumno->dni}} - {{$alumno->fullName}}</li>
                     @endif
                 @endforeach
                 </ul>
