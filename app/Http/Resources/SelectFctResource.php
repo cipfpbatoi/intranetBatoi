2c7d8594bb92db382b1350678fc5c520da8bd49c
igomis <igomis@cipfpbatoi.es>
2021-06-04 10:43:09 +0200

    Tasques i acces a nova

--- app/Http/Resources/SelectFctResource.php
+++ app/Http/Resources/SelectFctResource.php
@@ -16,7 +16,7 @@ class SelectFctResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'texto' => $this->Instructor->nombre.'('.$this->Colaboracion->Centro->nombre.')',
+            'texto' => $this->Instructor->nombre.'('.$this->Colaboracion->Centro->nombre.') Periode: '.$this->periode,
             'marked' => $this->marked
         ];
     }
