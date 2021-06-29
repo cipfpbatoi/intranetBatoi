2939590546e9cca36c537841d0afba162a79b22a
igomis <igomis@cipfpbatoi.es>
2021-06-29 10:45:59 +0200

    ultima pagina informes

--- resources/views/pdf/reunion/acta.blade.php
+++ resources/views/pdf/reunion/acta.blade.php
@@ -15,11 +15,6 @@
 @include('pdf.reunion.partials.punts')
 @include('pdf.reunion.partials.asistents')
 @include('pdf.reunion.partials.signatura')
-<div class="container" style="clear: both">
-    <br/>
-    <div style="width:50%;float:left">SIGNAT: {{$datosInforme->Responsable->nombre}}  {{$datosInforme->Responsable->apellido1}}  {{$datosInforme->Responsable->apellido2}}</div>
-    <div style="width:50%;float:right;text-align: right">{{strtoupper(config('contacto.poblacion'))}} A {{$datosInforme->hoy}}</div>
-</div>
 @include('pdf.partials.pie',['document'=>'actaReunio'])
 @endsection
 
