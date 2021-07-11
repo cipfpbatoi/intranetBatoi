6196190b3276c23f458bbf247c36957e9fbe52c8
igomis <igomis@cipfpbatoi.es>
2021-07-11 08:43:53 +0200

    Autorizaciones

--- resources/views/pdf/autorizacionMenores.blade.php
+++ resources/views/pdf/autorizacionMenores.blade.php
@@ -20,7 +20,7 @@
             <p>{!! config('contacto.poblacion') !!} a {{ FechaString() }}</p><br/><br/>
             <p>Firma / Signatura :</p>
         </div>
-        {!! QrCode::size(125)->generate(env('APP_URL','http://intranet.cipfpbatoi.es').'/autorizacion/'.$elemento->nia.'/actividad/'.$datosInforme->id); !!}
+        {!! QrCode::size(125)->generate(env('APP_URL','https://intranet.cipfpbatoi.es').'/autorizacion/'.$elemento->nia.'/actividad/'.$datosInforme->id); !!}
     </div>
     @endforeach
      @include('pdf.partials.pie',['document'=>'autoritzacioMenors'])
