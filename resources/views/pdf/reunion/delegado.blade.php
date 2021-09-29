fba2c3d7bfdd15acdd72ff689be4f67b29171916
igomis <igomis@cipfpbatoi.es>
2021-09-29 12:03:35 +0200

    delegado

--- resources/views/pdf/reunion/delegado.blade.php
+++ resources/views/pdf/reunion/delegado.blade.php
@@ -17,18 +17,18 @@
 </div>
 <br/><br/><br/>
 <div class="container" style="clear: both" >
-        <br/><strong> {{$todos[0]->descripcion}}:</strong>{{strip_tags($todos[0]->resumen)}}
-        <br/><strong> {{$todos[1]->descripcion}}:</strong>{{strip_tags($todos[1]->resumen)}}
-        @if (isset($todos[2]))<br/><strong> {{$todos[2]->descripcion}}:</strong>{{strip_tags($todos[2]->resumen)}}@endif
-        @if (isset($todos[3]))<br/><strong> {{$todos[3]->descripcion}}:</strong>{{strip_tags($todos[3]->resumen)}}@endif
-        @if (isset($todos[4]))<br/><strong>{{$todos[4]->descripcion}}:</strong> {{strip_tags($todos[4]->resumen)}}@endif
-        @if (isset($todos[5]))<br/><strong>{{$todos[5]->descripcion}}:</strong> {{strip_tags($todos[5]->resumen)}}@endif
-        @if (isset($todos[6]))<br/><strong>{{$todos[6]->descripcion}}:</strong> {{strip_tags($todos[6]->resumen)}}@endif
-        @if (isset($todos[7]))<br/><strong>{{$todos[7]->descripcion}}:</strong> {{strip_tags($todos[7]->resumen)}}@endif
+    <p><strong> {{$todos[0]->descripcion}}:</strong>{{strip_tags($todos[0]->resumen)}}</p>
+    <p><strong> {{$todos[1]->descripcion}}:</strong>{{strip_tags($todos[1]->resumen)}}</p>
+    @if (isset($todos[2]))<p><strong> {{$todos[2]->descripcion}}:</strong>{{strip_tags($todos[2]->resumen)}}</p>@endif
+    @if (isset($todos[3]))<p><strong> {{$todos[3]->descripcion}}:</strong>{{strip_tags($todos[3]->resumen)}}</p>@endif
+    @if (isset($todos[4]))<p><strong>{{$todos[4]->descripcion}}:</strong> {{strip_tags($todos[4]->resumen)}}</p>@endif
+    @if (isset($todos[5]))<p><strong>{{$todos[5]->descripcion}}:</strong> {{strip_tags($todos[5]->resumen)}}</p>@endif
+    @if (isset($todos[6]))<p><strong>{{$todos[6]->descripcion}}:</strong> {{strip_tags($todos[6]->resumen)}}</p>@endif
+    @if (isset($todos[7]))<p><strong>{{$todos[7]->descripcion}}:</strong> {{strip_tags($todos[7]->resumen)}}</p>@endif
 </div>
 <div class="container">
     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-    <div style="width:55%;float:left">CREAT PER: {{$datosInforme->Responsable->nombre}}&nbsp;&nbsp;&nbsp;</div>
+    <div style="width:55%;float:left">CREAT PER: {{$datosInforme->Responsable->fullNamw}}&nbsp;&nbsp;&nbsp;</div>
     <div style="width:45%;float:right;text-align: right">{{strtoupper(config('contacto.poblacion'))}} A {{$datosInforme->hoy}}</div>
 </div>
 @include('pdf.partials.pie',['document'=>'actaDelegat'])
