ae2d3ffc4975bccf8f5827bdb9c1c1c7a1470317
igomis <igomis@cipfpbatoi.es>
2021-10-20 09:38:59 +0200

    Carnet Professorat

--- app/Http/Controllers/Auth/HomeController.php
+++ app/Http/Controllers/Auth/HomeController.php
@@ -60,7 +60,7 @@ abstract class HomeController extends Controller
                     ->where('estado','>',1)
                     ->where('fueraCentro', '=', 1)
                     ->get();
-                $comisiones = Cache::remember('comisionesHui',now()->addHours(12),function(){
+                $comisiones = Cache::remember('comisionesHui',now()->addHours(6),function(){
                    return(Comision::with('profesor')->Dia(Hoy())->get());
                 });
 
