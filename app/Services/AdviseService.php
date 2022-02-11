c56a49bea5903cc6343e98be0eb2b953554fe32e
igomis <igomis@cipfpbatoi.es>
2022-02-11 10:12:47 +0100

    Creador

--- app/Services/AdviseService.php
+++ app/Services/AdviseService.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Intranet\Services;
+use Intranet\Entities\Alumno;
+use Intranet\Entities\Profesor;
+use Intranet\Notifications\mensajePanel;
 
 class AdviseService
 {
@@ -8,6 +11,33 @@ class AdviseService
     protected $element;
     protected $explanation;
     protected $link;
+
+    private static function receptor($id){
+        if (strlen($id) == 8) {
+            return Alumno::find($id);
+        }
+        return Profesor::find($id);
+    }
+
+    public static function avisa($id, $mensaje, $enlace = '#', $emisor = null)
+    {
+        $emisor = $emisor??AuthUser()->shortName;
+        $receptor = self::receptor($id);
+        $fecha = FechaString();
+        if ($emisor && $receptor) {
+            $receptor->notify(new mensajePanel(
+                ['motiu' => $mensaje,
+                    'emissor' => $emisor,
+                    'data' => $fecha,
+                    'enlace' => $enlace]));
+        } else {
+            AuthUser()->notify(new mensajePanel(
+                ['motiu' => "No trobe usuari $id",
+                    'emissor' => $emisor,
+                    'data' => $fecha,
+                    'enlace' => $enlace]));
+        }
+    }
 
 
     public function __construct($element,$message=null){
@@ -39,11 +69,11 @@ class AdviseService
     private function advise($dnis){
         if (is_array($dnis)) {
             foreach ($dnis as $dni) {
-                avisa($dni, $this->explanation, $this->link);
+                self::avisa($dni, $this->explanation, $this->link);
             }
         }
         else {
-            avisa($dnis, $this->explanation, $this->link);
+            self::avisa($dnis, $this->explanation, $this->link);
         }
     }
 
---
+++ config/avisos.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'secretario' => '020784763P',
+    'director' => '020823669K' ,
+    'vicedirector' => '021666224V',
+    'jefeEstudios' => '021657327K',
+    'jefeEstudios2' => '029071324Z',
+    'orientador' => ['021634142C','021669338A'],
+    'comissioIC' => ['021652470V','020823669K'],
+    'avisos' => [
+        'errores' => '021652470V',
+    ],
+];
