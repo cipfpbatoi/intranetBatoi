bfbce8ce2bb0ef9f102f689219845c2a43c69431
igomis <igomis@cipfpbatoi.es>
2022-02-14 12:16:40 +0100

    StatseService 1.2

--- app/Http/Controllers/MyMailController.php
+++ app/Http/Controllers/MyMailController.php
@@ -31,7 +31,7 @@ class MyMailController extends Controller
         else {
             $attach = null;
         }
-        if (strlen($request->contenido) < 50) {
+        if (strlen($request->contenido) < 50 && $request->editable == true) {
             Alert::danger('El contingut ha de ser més de 50 caracters');
         } else {
             $mail = new MyMail($request->to, $request->contenido, $request->toArray(), $attach);
