14d4f11e9b54626bf25632eee88f26d7d5bd7556
igomis <igomis@cipfpbatoi.es>
2022-02-21 18:55:30 +0100

    No avisa tutor

--- app/Services/AdviseTeacher.php
+++ app/Services/AdviseTeacher.php
@@ -2,72 +2,38 @@
 
 namespace Intranet\Services;
 
-
+use Intranet\Componentes\Mensaje;
 use Intranet\Entities\Hora;
 use Intranet\Entities\Horario;
-use Intranet\Entities\Profesor;
 
+use function hora, esMismoDia, nameDay;
 
 class AdviseTeacher
 {
-    protected $elemento;
-    protected $peopleToAdvise;
-    protected $emisor;
-    protected $name;
-
-
-    public function __construct($elemento,$peopleToAdvise=null){
-        $this->elemento = $elemento;
-        $this->emisor = $elemento->idProfesor;
-        $profesor = Profesor::find($elemento->idProfesor);
-        $this->name = $profesor->shortName;
-        if ($peopleToAdvise){
-            $this->peopleToAdvise = $peopleToAdvise;
-        } else {
-            if (count($grupos = self::gruposAfectados($this->elemento, $this->emisor)->toArray()) == 0) {
-               $this->peopleToAdvise = [];
-            } else {
-               $this->peopleToAdvise = self::profesoresAfectados($grupos, $this->emisor);
-            }
-        }
-    }
-
-    public function avisa(){
-        $mensaje = "No estaré en el centre des de " . $this->elemento->desde . " fins " . $this->elemento->hasta;
-        foreach ($this->peopleToAdvise as $people){
-            avisa($people->idProfesor, $mensaje, '#', $this->name);
-        }
-    }
-
-    public static function exec($elemento){
-        $at = new AdviseTeacher($elemento);
-        $at->avisa();
-    }
-
-/**
-    protected function avisaProfesorat($elemento, $mensaje = null, $idEmisor = null, $emisor = null)
+    public static function exec($elemento, $mensaje = null, $idEmisor = null, $emisor = null)
     {
         $mensaje = $mensaje ? $mensaje : "No estaré en el centre des de " . $elemento->desde . " fins " . $elemento->hasta;
         $idEmisor = $idEmisor ? $idEmisor : $elemento->idProfesor;
 
-        if (count($grupos = $this->gruposAfectados($elemento, $idEmisor)->toArray()) == 0) {
+        if (count($grupos = self::gruposAfectados($elemento, $idEmisor)->toArray()) == 0) {
             return;
         }
-        
-        foreach ($this->profesoresAfectados($grupos, $idEmisor) as $profesor) {
-            avisa($profesor->idProfesor, $mensaje, '#', $emisor);
+
+        foreach (self::profesoresAfectados($grupos, $idEmisor) as $profesor) {
+            Mensaje::send($profesor->idProfesor, $mensaje, '#', $emisor);
         }
     }
-*/
 
-    protected static function profesoresAfectados($grupos,$emisor){
+    private static function profesoresAfectados($grupos, $emisor)
+    {
         return Horario::distinct()
-                ->select('idProfesor')
-                ->whereIn('idGrupo', $grupos)
-                ->where('idProfesor', '<>', $emisor)
-                ->get();
+            ->select('idProfesor')
+            ->whereIn('idGrupo', $grupos)
+            ->where('idProfesor', '<>', $emisor)
+            ->get();
     }
-    protected static function gruposAfectados($elemento, $idProfesor)
+
+    private static function gruposAfectados($elemento, $idProfesor)
     {
         if (!esMismoDia($elemento->desde, $elemento->hasta)) {
             return (Horario::distinct()
@@ -87,11 +53,11 @@ class AdviseTeacher
                 ->whereIn('sesion_orden', $horas)
                 ->get());
         }
-        
+
         return collect();
     }
 
-    protected static function horasAfectadas($elemento)
+    private static function horasAfectadas($elemento)
     {
         if (!isset($elemento->dia_completo)) {
             return Hora::horasAfectadas(hora($elemento->desde), hora($elemento->hasta));
@@ -100,6 +66,6 @@ class AdviseTeacher
             return Hora::horasAfectadas('07:00', '23:00');
         }
         return Hora::horasAfectadas($elemento->hora_ini, $elemento->hora_fin);
-        
+
     }
-} 
+}
