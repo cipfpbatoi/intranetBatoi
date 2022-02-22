8b3cafc81c1769ac74dbbd488b88de60ecb6905c
juanseguravasco <jsegura@cipfpbatoi.es>
2022-02-22 11:44:55 +0100

    Juan - quitar plazo de cambio de horario

--- resources/views/horario/profesor-cambiar.blade.php
+++ resources/views/horario/profesor-cambiar.blade.php
@@ -3,7 +3,6 @@
 	<p>Propuesta de modificación del horario que deberá ser aprobada por jefatura de estudios.</p>
 	<p>Sobre tu horario original cambia las horas arrástrandolas a donde desees y cuando lo tengas pulsa el botón de 'Guardar cambios'.</p>
 	<p>Cuando lo haya aprobado jefatura te aparecerá tu horario cambiado.</p>
-	<p>El plazo para proponer el cambio de horario es hasta 31 de enero.</p>
 	<p>En el campo observaciones debes poner el nº de horas que liberas y a qué tareas dedicarás cada una de dichas horas.</p>
 	<strong>Estado: </strong><input type="text" id="estado" readonly><br>
 	<button id="init">Iniciar Procedimiento</button>
