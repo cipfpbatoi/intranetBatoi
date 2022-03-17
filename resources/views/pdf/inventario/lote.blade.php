4099d29ac772b40c24a829176c03d080d3999b48
igomis <igomis@cipfpbatoi.es>
2022-03-17 16:42:16 +0100

    Etiquetes

--- resources/views/pdf/inventario/lote.blade.php
+++ resources/views/pdf/inventario/lote.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.label')
 @section('content')
     @foreach ($todos as $material)
-        <div class="visible-print text-center" style="float:left;width:8cm;height:3.7cm;margin: 0cm;padding-left: 0.5cm;padding-top: 0.5cm">
+        <div class="visible-print text-center" style="float:left;width:8.2cm;height:4.15cm;margin: 0cm;padding-left: 0.5cm;padding-top: 0.5cm">
             @include('pdf.inventario.partials.fitxa')
         </div>
     @endforeach
--- resources/views/pdf/inventario/partials/fitxa.blade.php
+++ resources/views/pdf/inventario/partials/fitxa.blade.php
@@ -1,13 +1,13 @@
 <div style="float:left; width: 3cm">
 {!! QrCode::size(115)->generate(env('APP_URL','http://intranet.cipfpbatoi.es').'/inventario/'.$material->id.'/edit'); !!}
 </div>
-<div style="float:left; width: 4.70cm; font-size: medium; margin-left: 0.3cm;text-align: left">
+<div style="float:left; width: 4.90cm; font-size: medium; margin-left: 0.3cm;text-align: left">
         <strong>Id: {{ $material->id }}</strong>
             @if (($material->nserieprov) && ($material->nserieprov != 'null' ))
                 ({{ $material->nserieprov }})
             @endif
         <br/>
-        {{ ucfirst(strtolower($material->descripcion)) }}<br/>
+        {{ ucfirst(strtolower(substr($material->descripcion,0,22))) }}<br/>
         @isset ($material->marca)
                 {{ucfirst(strtolower($material->marca))}}
                 @isset ($material->modelo)
