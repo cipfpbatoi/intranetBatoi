ce61fbc35ba25b2343321022f2f330a080ba35b2
igomis <igomis@cipfpbatoi.es>
2022-06-06 13:28:55 +0200

    AnexosController.php

--- app/Http/Controllers/AnexosController.php
+++ app/Http/Controllers/AnexosController.php
@@ -50,8 +50,7 @@ class AnexosController extends Controller
     public function index(){
         $link = $this->link."application/2021/student/023907686Z/document/10";
         if ($this->login()){
-            $response = Http::withToken($this->token)->attach('file',file_get_contents('/var/www/html/intranetBatoi/storage/app/public/adjuntos/al
-umnofctaval/1294/A5.pdf'))->post($link);
+            $response = Http::withToken($this->token)->attach('file',file_get_contents('/var/www/html/intranetBatoi/storage/app/public/adjuntos/alumnofctaval/1294/A5.pdf'))->post($link);
             dd($response);
         } else {
             dd('Sense connexió');
