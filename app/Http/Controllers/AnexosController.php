3a00c042228e887d5c5e4e453a4f0194a8dccde1
igomis <igomis@cipfpbatoi.es>
2022-06-13 20:39:46 +0200

    Malament

--- app/Http/Controllers/AnexosController.php
+++ app/Http/Controllers/AnexosController.php
@@ -5,30 +5,8 @@ namespace Intranet\Http\Controllers;
 
 
 use Illuminate\Support\Facades\Http;
-use Intranet\Entities\Articulo;
-use Intranet\Entities\ArticuloLote;
-use Intranet\Entities\Espacio;
-use Intranet\Entities\Material;
-use Intranet\Entities\Lote;
-use Intranet\Entities\Documento;
-use Intranet\Entities\Empresa;
-use Illuminate\Support\Facades\Session;
-use Intranet\Entities\Grupo;
-use Intranet\Entities\Menu;
-use Intranet\Entities\Poll\Poll;
-use Intranet\Entities\Poll\VoteAnt;
-use Intranet\Entities\Programacion;
 use DB;
-use Intranet\Entities\Departamento;
-use Styde\Html\Facades\Alert;
-use Intranet\Entities\Profesor;
-use Illuminate\Support\Facades\Storage;
-use Intranet\Jobs\SendEmail;
-use Intranet\Entities\AlumnoGrupo;
-use Intranet\Entities\Colaboracion;
-use Intranet\Entities\Poll\Vote;
-use Intranet\Entities\Fct;
-use Illuminate\Http\Request;
+
 
 
 /**
@@ -37,7 +15,6 @@ use Illuminate\Http\Request;
  */
 class AnexosController extends Controller
 {
-    const DIRECTORIO_GESTOR = 'gestor/Empresa/';
     private $user,$pass,$link,$token;
 
 
@@ -48,9 +25,16 @@ class AnexosController extends Controller
     }
 
     public function index(){
-        $link =
+        $link = $this->link."application/2021/student/023907686Z/document/10";
         if ($this->login()){
-            $reponse = Http::post()
+            $response = Http::withToken($this->token)
+                ->attach('file',file_get_contents('/var/www/html/intranetBatoi/storage/app/public/adjuntos/alumnofctaval/1294/A5.pdf'))
+                ->post($link);
+
+            dd($response);
+
+        } else {
+            dd('Sense connexió');
         }
 
 
@@ -60,7 +44,7 @@ class AnexosController extends Controller
         $link = $this->link."login_check";
 
         $response = Http::post($link,['username'=>$this->user,'password'=>$this->pass]);
-        if (isset($response['token']){
+        if (isset($response['token'])){
             $this->token = $response['token'];
             return 1;
         } else {
