9a26509d838fa1bab7c1af58906ec275eec45e44
igomis <igomis@cipfpbatoi.es>
2022-06-28 14:51:05 +0200

    Pujar Annexes des de la pantalla

--- app/Jobs/UploadFiles.php
+++ app/Jobs/UploadFiles.php
@@ -7,9 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Mail;
-use Intranet\Mail\Comunicado;
+
 
 
 class UploadFiles implements ShouldQueue
