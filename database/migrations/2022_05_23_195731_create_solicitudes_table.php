a11655a9bdfba8ca793f70a084baf53422f759af
igomis <igomis@cipfpbatoi.es>
2022-08-29 17:55:19 +0200

    Autenticació amb email professor i alumne

--- database/migrations/2022_05_23_195731_create_solicitudes_table.php
+++ database/migrations/2022_05_23_195731_create_solicitudes_table.php
@@ -15,15 +15,15 @@ class CreateSolicitudesTable extends Migration
     {
         Schema::create('solicitudes', function (Blueprint $table) {
             $table->id();
-            $table->string('idAlumno', 8)->collation('utf8_unicode_ci')->index('solicitudes_idalumno_foreign');
-            $table->string('idProfesor', 10)->collation('utf8_unicode_ci')->index('solicitudes_idprofesor_foreign');
+            $table->string('idAlumno', 8)->collation('utf8mb4_unicode_ci')->index('solicitudes_idalumno_foreign');
+            $table->string('idProfesor', 10)->collation('utf8mb4_unicode_ci')->index('solicitudes_idprofesor_foreign');
             $table->text('text1', 65535);
             $table->text('text2', 65535)->nullable();
             $table->text('text3', 65535)->nullable();
             $table->date('fecha');
             $table->date('fechasolucion')->nullable();
             $table->boolean('estado')->default(0);
-            $table->string('idOrientador', 10)->collation('utf8_unicode_ci')->nullable()->index('solicitudes_idorientador_foreign');;
+            $table->string('idOrientador', 10)->collation('utf8mb4_unicode_ci')->nullable()->index('solicitudes_idorientador_foreign');;
         });
     }
 
