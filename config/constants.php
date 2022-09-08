49aac01095f3df2b07e036907fcd290f88a4cd39
igomis <igomis@cipfpbatoi.es>
2022-09-08 17:47:23 +0200

    Exempcio

--- config/constants.php
+++ config/constants.php
@@ -3,7 +3,7 @@
 return [
     
     'codigoGuardia' => '3249454',
-    'version' => ['v2_36'],
+    'version' => ['v2_40'],
     'modulosNoLectivos' => ['TU01CF','TU02CF'],
     'modulosSinProgramacion' => ['TU01CF','TU02CF'],
     //'pdf' => 'DomPdf',
