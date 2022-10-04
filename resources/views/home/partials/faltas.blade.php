eb2c37ac146067d588b8495669741d6e6421452d
igomis <igomis@cipfpbatoi.es>
2022-10-04 17:27:09 +0200

    Pantalla Principal

--- resources/views/home/partials/faltas.blade.php
+++ resources/views/home/partials/faltas.blade.php
@@ -1,42 +1,46 @@
 <ul class="messages">
     @foreach ($faltas as $falta)
-    <li>
-        @if ($falta->profesor->activo)
-        <img src="/img/ill.png" class="avatar" alt="Avatar">
-        <div class="message_date">
-            @if ($falta->dia_completo)
-                HUI
-            @else
-                {{$falta->hora_ini}} - {{$falta->hora_fin}}
+            @if ($falta->profesor->activo)
+                <li>
+                <img src="/img/ill.png" class="avatar" alt="Avatar">
+                <div class="message_date">
+                    @if ($falta->dia_completo)
+                        HUI
+                    @else
+                        {{$falta->hora_ini}} - {{$falta->hora_fin}}
+                    @endif
+                </div>
+                <div class="message_wrapper">
+                    <h4 class="heading">{{$falta->profesor->fullName}}</h4>
+                    <br />
+                </div>
+                </li>
             @endif
-        </div>
-        <div class="message_wrapper">
-            <h4 class="heading">{{$falta->profesor->fullName}}</h4>
-            <br />
-        </div>
-        @endif
     @endforeach
     @foreach ($hoyActividades as $actividad)
         @foreach ($actividad->profesores as $profesor)
-            <img src="/img/actividad.png" class="avatar" alt="Avatar">
-            <div class="message_date">
-                {{hora($actividad->desde)}} - {{hora($actividad->hasta)}}
-            </div>
-            <div class="message_wrapper">
-                <h4 class="heading">{{$profesor->fullName}}</h4>
-                <br />
-            </div>
+                <li>
+                    <img src="/img/actividad.png" class="avatar" alt="Avatar">
+                    <div class="message_date">
+                        {{hora($actividad->desde)}} - {{hora($actividad->hasta)}}
+                    </div>
+                    <div class="message_wrapper">
+                        <h4 class="heading">{{$profesor->fullName}}</h4>
+                        <br />
+                    </div>
+                </li>
+            @endforeach
         @endforeach
-    @endforeach
-    @foreach ($comisiones as $comision)
-        <img src="/img/coche.png" class="avatar" alt="Avatar">
-        <div class="message_date">
-            {{hora($comision->desde)}} - {{hora($comision->hasta)}}
-        </div>
-        <div class="message_wrapper">
-            <h4 class="heading">{{$comision->profesor->fullName}}</h4>
-            <br />
-        </div>
-    </li>
+        @foreach ($comisiones as $comision)
+            <li>
+                <img src="/img/coche.png" class="avatar" alt="Avatar">
+                <div class="message_date">
+                    {{hora($comision->desde)}} - {{hora($comision->hasta)}}
+                </div>
+                <div class="message_wrapper">
+                    <h4 class="heading">{{$comision->profesor->fullName}}</h4>
+                    <br />
+                </div>
+            </li>
     @endforeach
 </ul>
