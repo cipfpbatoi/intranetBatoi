36cb6af3006ccc0fe2eb78b6a161466aca37cfb0
igomis <igomis@cipfpbatoi.es>
2022-10-04 16:42:23 +0200

    Pantalla principal

--- resources/views/home/partials/tasks.blade.php
+++ resources/views/home/partials/tasks.blade.php
@@ -1,39 +1,39 @@
 <ul class="messages">
+  @foreach ($tasks as $task)
     <li>
-        @foreach ($tasks as $task)
-            @if (!$task->valid || $task->vencimiento > Hoy())
-                @if ($task->vencimiento <= Hoy())
-                    <img src="/img/warning.png" class="avatar" alt="Avatar"/>
+        @if (!$task->valid || $task->vencimiento > Hoy())
+            @if ($task->vencimiento <= Hoy())
+                <img src="/img/warning.png" class="avatar" alt="Avatar"/>
+            @else
+                @if ($task->informativa)
+                    <img src="/img/informacion.jpeg" class="avatar" alt="Avatar"/>
                 @else
-                    @if ($task->informativa)
-                        <img src="/img/informacion.jpeg" class="avatar" alt="Avatar"/>
-                    @else
-                        <img src="/img/task.png" class="avatar" alt="Avatar">
-                    @endif
+                    <img src="/img/task.png" class="avatar" alt="Avatar">
                 @endif
-                <div class="message_date" title="venciment">
-                        <h4 class="date text-info">{{day($task->vencimiento)}}</h4>
-                        <p class="month">{{month($task->vencimiento)}}</p>
-                </div>
-
-                <div class="message_wrapper">
-                    <h4 class="heading">
-                        @if ($task->fichero)
-                            <a href="/storage/{{$task->fichero}}" title="Més Informació">{{$task->descripcion}}&nbsp;&nbsp;&nbsp;&nbsp;
-                                <i class="fa fa-clipboard"></i></a> &nbsp;
-                        @else
-                            <a href="{{$task->enlace}}" title="Més Informació">{{$task->descripcion}}&nbsp;&nbsp;&nbsp;&nbsp;
-                                <i class="fa fa-clipboard"></i></a> &nbsp;
-                        @endif    &nbsp;&nbsp;&nbsp;
-                        @if ($task->valid)
-                            <a href="/task/{{$task->id}}/check" title="Tasca revisada"><i class="fa fa-check-square-o"></i></a>
-                        @else
-                            <a href="/task/{{$task->id}}/check" title="Tasca revisada"><i class="fa fa-square-o"></i></a>
-                        @endif
-                    </h4>
-                    <br />
-                </div>
             @endif
-        @endforeach
+            <div class="message_date" title="venciment">
+                    <h4 class="date text-info">{{day($task->vencimiento)}}</h4>
+                    <p class="month">{{month($task->vencimiento)}}</p>
+            </div>
+
+            <div class="message_wrapper">
+                <h4 class="heading">
+                    @if ($task->fichero)
+                        <a href="/storage/{{$task->fichero}}" title="Més Informació">{{$task->descripcion}}&nbsp;&nbsp;&nbsp;&nbsp;
+                            <em class="fa fa-clipboard"></em></a> &nbsp;
+                    @else
+                        <a href="{{$task->enlace}}" title="Més Informació">{{$task->descripcion}}&nbsp;&nbsp;&nbsp;&nbsp;
+                            <em class="fa fa-clipboard"></em></a> &nbsp;
+                    @endif    &nbsp;&nbsp;&nbsp;
+                    @if ($task->valid)
+                        <a href="/task/{{$task->id}}/check" title="Tasca revisada"><em class="fa fa-check-square-o"></em></a>
+                    @else
+                        <a href="/task/{{$task->id}}/check" title="Tasca revisada"><em class="fa fa-square-o"></em></a>
+                    @endif
+                </h4>
+                <br />
+            </div>
+        @endif
     </li>
+    @endforeach
 </ul>
