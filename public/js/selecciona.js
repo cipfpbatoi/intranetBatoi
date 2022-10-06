c68bf1b0611e83bc4d6b5607e9a39975364505c3
igomis <igomis@cipfpbatoi.es>
2022-10-06 11:43:56 +0200

    Selecciona all

--- public/js/selecciona.js
+++ public/js/selecciona.js
@@ -12,17 +12,52 @@ $(".selecciona").on("click",function(event){
     })
         .then(function (result) {
             pintaTablaSeleccion(result.data);
-        }, function (result) {
+         }, function (result) {
             console.log("La solicitud no se ha podido completar.");
         });
 });
+
+
+$( ".btn-primary" ).click(function() {
+    event.preventDefault();
+    $("#checkall").prop('checked',false);
+    $( "#formSeleccion" ).submit();
+});
+
 function pintaTablaSeleccion(newOptions){
     var $el = $("#tableSeleccion");
     var checked;
+    var emptys = 0;
+    var checks = 0;
     $el.empty(); // remove old options
     $.each(newOptions, function (key, value) {
-        if (value.marked == true || value.marked == null) checked = ' checked';
-        else checked = '';
-        $el.append($("<tr><td><input type='checkbox' name='"+value.id+"'"+checked+"> "+value.texto+"</td></tr>"));
+        if (value.marked  || value.marked == null){
+            checked = ' checked';
+            checks += 1;
+        }
+        else {
+            checked = '';
+            emptys += 1;
+        }
+        $el.append($("<tr><td><input type='checkbox' class='elements' name='"+value.id+"'"+checked+"> "+value.texto+"</td></tr>"));
     });
+    if (checks > emptys) {
+        checked = 'checked';
+    } else {
+        checked = ' ';
+    }
+    $el.append($("<tr><td>-------------------------------</td></tr>"))
+    $el.append($("<tr><td><div id='divCheckAll'><input type='checkbox' name='checkall' id='checkall' onClick='check_uncheck_checkbox(this.checked);'"+checked+"/>Check All</div></td></tr>"));
+}
+
+function check_uncheck_checkbox(isChecked) {
+    if(isChecked) {
+        $('input.elements').each(function() {
+            this.checked = true;
+        });
+    } else {
+        $('input.elements').each(function() {
+            this.checked = false;
+        });
+    }
 }
