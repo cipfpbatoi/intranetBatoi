01b596699ef57593a541760ff8ed8a688e0b178f
igomis <igomis@cipfpbatoi.es>
2022-12-27 08:45:39 +0100

    Html

--- public/js/Colaboracion/index.js
+++ public/js/Colaboracion/index.js
@@ -6,6 +6,16 @@ $('#datatable').DataTable( {
     },
     deferRender: true,
     responsive: true,
+    rowCallback: function (row,data){
+        if (data[3].includes('Col·labora')) {
+            $(row).addClass('bg-green')
+        } else {
+            if (data[3].includes('No col')) {
+                $(row).addClass('bg-red')
+            }
+
+        }
+    },
     columnDefs: [
         { responsivePriority: 1, targets: -1},
     ]
