5fa105063747845ec4641258778c0a69f7930402
igomis <igomis@cipfpbatoi.es>
2023-01-31 11:51:44 +0100

    Empresa Alumno

--- resources/views/colaboracion/partials/colaboradores.blade.php
+++ resources/views/colaboracion/partials/colaboradores.blade.php
@@ -1,10 +1,10 @@
 <ul class="messages colaboracion">
-    @foreach ($fcts as $fct)
+    @foreach ($fcts??[] as $fct)
         <ul class="messages fct">
-            <em class="fa fa-credit-card"></em> {{$fct->Instructor->id}}
-            <em class="fa fa-user user-profile-icon"></em> {{$fct->Instructor->Nombre}}
-            <em class="fa fa-envelope"></em> {{$fct->Instructor->email}}
-            <em class="fa fa-phone"></em> {{$fct->Instructor->telefono}}
+            <em class="fa fa-credit-card"></em> {{$fct->Instructor->id??'No hi ha instructor'}}
+            <em class="fa fa-user user-profile-icon"></em> {{$fct->Instructor->Nombre??''}}
+            <em class="fa fa-envelope"></em> {{$fct->Instructor->email??''}}
+            <em class="fa fa-phone"></em> {{$fct->Instructor->telefono??''}}
             <form action='/fct/{!!$fct->id!!}/modificaHoras' method='post'>
                 @csrf
                 @foreach ($fct->Colaboradores->sortBy('surnames') as $instructor)
@@ -28,7 +28,12 @@
                 @endforeach
                 @if(userIsAllow(config('roles.rol.tutor')))
                     <div class="message_wrapper">
-                        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#AddInstructor">
+                        <button
+                                type="button"
+                                class="btn btn-secondary"
+                                data-toggle="modal"
+                                data-target="#AddInstructor"
+                        >
                                 @lang("messages.generic.anadir") @lang("models.modelos.Colaborador")
                         </button>
                         <input type='submit' class="btn btn-secondary"
