62651c372d908aa4f299a3d0fb7795a0eed41629
igomis <igomis@cipfpbatoi.es>
2023-02-16 11:21:29 +0100

    Fussion centres de treball

--- app/Http/Controllers/SaoA2Controller.php
+++ app/Http/Controllers/SaoA2Controller.php
@@ -3,6 +3,8 @@
 namespace Intranet\Http\Controllers;
 
 use Exception;
+use Facebook\WebDriver\Firefox\FirefoxDriver;
+use Facebook\WebDriver\Firefox\FirefoxProfile;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Intranet\Entities\AlumnoFct;
@@ -17,20 +19,38 @@ use Styde\Html\Facades\Alert;
 class SaoA2Controller extends SaoController
 {
 
+    private function closeWindows(RemoteWebDriver $driver, $window) {
+        var_dump($driver->getWindowHandles());
 
-    public function download_file_from_fcts($password)
+        return $driver;
+    }
+
+    public function download_file_from_fcts(RemoteWebDriver $driver)
     {
-        $fctAl = AlumnoFct::misFcts()->activa()->first();
+        //$fctAl = AlumnoFct::misFcts()->activa()->first();
+
 
         foreach (AlumnoFct::misFcts()->activa()->get() as $fctAl) {
             try {
-                $driver = RemoteWebDriver::create($this->serverUrl, DesiredCapabilities::firefox());
-                $this->login($driver, $password);
+                $driver->get("https://foremp.edu.gva.es/inc/ajax/generar_pdf.php?doc=3&centro=59&idFct=$fctAl->idSao");
+
+                /*
+                dd( $driver->getWindowHandles());
+
+                $windowsHandles =;
+                var_dump($windowsHandles);
+                foreach ($windowsHandles as $windowHandle) {
+                    if ($window != $windowHandle) {
+                        $driver->switchTo()->window($windowHandle);
+                        $driver->close();
+                        var_dump('Tancada finestra '.$windowHandle);
+                    }
+                }
+                $driver->switchTo()->window($window);
+                sleep(1);*/
+            } catch (\Throwable $exception) {
+                $driver->get('https://foremp.edu.gva.es/index.php?op=2&subop=0');
                 sleep(1);
-                $driver->get("https://foremp.edu.gva.es/inc/ajax/generar_pdf.php?doc=2&centro=59&idFct=$fctAl->idSao");
-                sleep(1);
-                $driver->close();
-            } catch (Exception $exception) {
                 Alert::info($exception->getMessage());
             }
         }
@@ -41,17 +61,39 @@ class SaoA2Controller extends SaoController
         $grupo = Grupo::where('tutor', AuthUser()->dni)->first();
         $ciclo = $grupo->idCiclo??null;
 
+        $profile = new FirefoxProfile();
+        $caps = DesiredCapabilities::firefox();
+
+        $profile->setPreference('browser.download.folderList', 2);
+        $profile->setPreference('browser.download.dir', __DIR__.'/Downloads');
+        $profile->setPreference('browser.helperApps.neverAsk.saveToDisk', 'application/pdf');
+        $profile->setPreference('pdfjs.enabledCache.state', false);
+        $profile->setPreference('modifyheaders.headers.count', 1);
+        $profile->setPreference("modifyheaders.headers.action0", "Add");
+        $profile->setPreference("modifyheaders.headers.name0", "Content-Disposition"); # Set here the name of the header
+        $profile->setPreference("modifyheaders.headers.value0", "inline"); # Set here the value of the header
+        $profile->setPreference("modifyheaders.headers.enabled0", true);
+        $profile->setPreference("modifyheaders.config.active", true);
+        $profile->setPreference("modifyheaders.config.alwaysOn", true);
+        $caps->setCapability('firefox_profile', $profile);
+
+
         if (!$ciclo) {
             Alert::danger('No eres tutor');
             return redirect(route('alumnofct.index'));
         } else {
+            $driver = RemoteWebDriver::create($this->serverUrl, $caps);
+            $driver->manage()->timeouts()->pageLoadTimeout(2);
             try {
-                $this->download_file_from_fcts($password);
+                $this->login($driver, $password);
+                $this->download_file_from_fcts($driver);
+                $driver->close();
             } catch (Exception $e) {
                 Alert::warning($e->getMessage());
-                return redirect(route('alumnofct.index'));
+                $driver->close();
             }
         }
+        return redirect(route('alumnofct.index'));
     }
 
 }
