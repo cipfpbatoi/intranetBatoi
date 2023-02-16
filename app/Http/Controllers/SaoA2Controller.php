25f32637b338e67e92c92b95d1b9443bcce30cbd
igomis <igomis@cipfpbatoi.es>
2023-02-16 08:33:28 +0100

    Fussion centres de treball

--- app/Http/Controllers/SaoA2Controller.php
+++ app/Http/Controllers/SaoA2Controller.php
@@ -5,16 +5,8 @@ namespace Intranet\Http\Controllers;
 use Exception;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
-use Facebook\WebDriver\WebDriverBy;
-use Illuminate\Http\Request;
 use Intranet\Entities\AlumnoFct;
-use Intranet\Entities\Centro;
-use Intranet\Entities\Ciclo;
-use Intranet\Entities\Colaboracion;
-use Intranet\Entities\Fct;
 use Intranet\Entities\Grupo;
-use Intranet\Entities\Empresa;
-use Intranet\Entities\Instructor;
 use Styde\Html\Facades\Alert;
 
 
@@ -34,9 +26,9 @@ class SaoA2Controller extends SaoController
             try {
                 $driver->get("https://foremp.edu.gva.es/inc/ajax/generar_pdf.php?doc=2&centro=59&idFct=$fctAl->idSao");
                 sleep(1);
-                //$driver->get('https://foremp.edu.gva.es/index.php?op=2&subop=0');
-                //sleep(1);
-            } catch (\Throwable $exception) {
+                $driver->get('https://foremp.edu.gva.es/index.php?op=2&subop=0');
+                sleep(1);
+            } catch (Exception $exception) {
                 $driver->get('https://foremp.edu.gva.es/index.php?op=2&subop=0');
                 sleep(1);
                 Alert::info($exception->getMessage());
