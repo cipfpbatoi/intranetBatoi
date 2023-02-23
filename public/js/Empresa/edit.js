37cd9619ba0b9ab674f47f7e7ed39de94d06ce30
igomis <igomis@cipfpbatoi.es>
2023-02-23 12:37:52 +0100

    DropZone

--- public/js/Empresa/edit.js
+++ public/js/Empresa/edit.js
@@ -3,25 +3,6 @@
 $(function () {
     if ($("#dni").text() === $("#creador_id").val() || ($('#rol').text() % 41 == 0) ||
         ($('#concierto_id').val() == '')) {
-        if ($("#europa_id").is(":checked")) {
-            $('#sao_id').prop('disabled', true);
-            $('#concierto_id').prop('disabled', true);
-        } else {
-            $('#sao_id').prop('disabled', false);
-        }
-        $('#europa_id').change(function () {
-            if ($("#europa_id").is(":checked")) {
-                $('#sao_id').prop('disabled', true);
-                $('#concierto_id').prop('disabled', true);
-            } else {
-                $('#sao_id').prop('disabled', false);
-                if ($("#sao_id").is(":checked")) {
-                    $('#concierto_id').prop('disabled', false);
-                } else {
-                    $('#concierto_id').prop('disabled', true);
-                }
-            }
-        });
         if ($("#sao_id").is(":checked")) {
             $('#concierto_id').prop('disabled', false);
         } else {
