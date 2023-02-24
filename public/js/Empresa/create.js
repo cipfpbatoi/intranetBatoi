5c1fff69ae731114597d5a60e075f4ccc4b9a037
igomis <igomis@cipfpbatoi.es>
2023-02-24 11:34:15 +0100

    Erasmus

--- public/js/Empresa/create.js
+++ public/js/Empresa/create.js
@@ -1,23 +1,6 @@
 'use strict';
 
 $(function () {
-    $('#europa_id').change(function () {
-        var check = $("#europa_id").is(":checked");
-        if (check){
-            $('#sao_id').prop('disabled', true);
-            $('#concierto_id').prop('disabled', true);
-        }
-        else {
-            $('#sao_id').prop('disabled', false);
-            var check = $("#sao_id").is(":checked");
-            if (check){
-                $('#concierto_id').prop('disabled', false);
-            }
-            else {
-                $('#concierto_id').prop('disabled', true);
-            }
-        }
-    });
     $('#sao_id').change(function () {
         var check = $("#sao_id").is(":checked");
         if (check){
@@ -39,5 +22,4 @@ $(function () {
                 alert("Error: CIF duplicat amb l'empresa "+result.data[0].nombre+' de concert '+result.data[0].concierto);
             });
     });
-
 });
