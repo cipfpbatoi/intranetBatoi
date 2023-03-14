5c238e737437911dc33cf0dc6f3e80c2281dd555
igomis <igomis@cipfpbatoi.es>
2023-03-14 17:19:52 +0100

    Unlink FCT

--- app/Http/Controllers/SaoAnnexesController.php
+++ app/Http/Controllers/SaoAnnexesController.php
@@ -24,7 +24,12 @@ class SaoAnnexesController
         try {
             $driver = SeleniumService::loginSAO(AuthUser()->dni, $password);
             $alumnes = [];
-            foreach (AlumnoFctAval::realFcts()->whereNotNull('idSao')->activa()->where('pg0301', 0)->get() as $fct) {
+            foreach (AlumnoFctAval::realFcts()
+                         ->where('beca', 0)
+                         ->whereNotNull('idSao')
+                         ->activa()
+                         ->where('pg0301', 0)
+                         ->get() as $fct) {
                 $find = Adjunto::where('size', 1024)->where('route', 'alumnofctaval/'.$fct->id)->count();
                 if ($fct->idSao) {
                     if (!$find) {
