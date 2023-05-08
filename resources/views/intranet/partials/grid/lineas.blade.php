7f7b17075b3423254cff4ab14c9495ea1143ed06
igomis <igomis@cipfpbatoi.es>
2023-05-08 19:40:33 +0200

    Annexes per signar

--- resources/views/intranet/partials/grid/lineas.blade.php
+++ resources/views/intranet/partials/grid/lineas.blade.php
@@ -3,7 +3,15 @@
         @foreach ($pestana->getRejilla() as $item)
             @if (substr($item,0,1) == 'L') @php $long = 200; @endphp
             @else @php $long = 90; @endphp @endif
-            <td><span class='input' name='{{ $item }}'>@if (isset($elemento->leido)&&!($elemento->leido))<strong> {!!  substr($elemento->$item,0,$long) !!} </strong> @else  {!!  mb_substr($elemento->$item,0,$long)  !!} @endif</span></td>
+            <td>
+                <span class='input' name='{{ $item }}'>
+                    @if (isset($elemento->leido)&&!($elemento->leido))
+                        <strong> {!!  mb_substr($elemento->$item,0,$long) !!} </strong>
+                    @else
+                        {!!  mb_substr($elemento->$item,0,$long)  !!}
+                    @endif
+                </span>
+            </td>
         @endforeach
         <td>
             <span class="botones">
