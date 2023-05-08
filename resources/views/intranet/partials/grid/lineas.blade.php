5cd54bcb49de6f8eabfe425d047a6befd9c91a5b
igomis <igomis@cipfpbatoi.es>
2023-05-08 19:47:50 +0200

    Annexes per signar

--- resources/views/intranet/partials/grid/lineas.blade.php
+++ resources/views/intranet/partials/grid/lineas.blade.php
@@ -1,8 +1,12 @@
+@php(mb_internal_encoding("UTF-8"))
 @foreach ($panel->getElementos($pestana) as $elemento)
     <tr class="lineaGrupo {{$elemento->class??''}} " id='{{$elemento->getkey()}}'>
         @foreach ($pestana->getRejilla() as $item)
-            @if (substr($item,0,1) == 'L') @php $long = 200; @endphp
-            @else @php $long = 90; @endphp @endif
+            @if (substr($item,0,1) == 'L')
+                @php($long = 200)
+            @else
+                @php($long = 90)
+            @endif
             <td>
                 <span class='input' name='{{ $item }}'>
                     @if (isset($elemento->leido)&&!($elemento->leido))
