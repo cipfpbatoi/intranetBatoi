78b67d88458a849244aebb1221a0a7e97917d8e4
igomis <igomis@cipfpbatoi.es>
2023-05-17 19:03:57 +0200

    Contactos

--- config/constants.php
+++ config/constants.php
@@ -3,7 +3,7 @@
 return [
     
     'codigoGuardia' => '3249454',
-    'version' => ['v2_50'],
+    'version' => ['v3_00'],
     'modulosNoLectivos' => ['TU01CF','TU02CF'],
     'modulosSinProgramacion' => ['TU01CF','TU02CF'],
     //'pdf' => 'DomPdf',
