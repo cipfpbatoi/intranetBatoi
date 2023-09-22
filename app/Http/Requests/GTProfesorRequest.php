bae02e474df034c0cf8391298497ba56047762ca
igomis <igomis@cipfpbatoi.es>
2023-09-22 12:55:18 +0200

    MaterialBaja

---
+++ app/Http/Requests/GTProfesorRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Intranet\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class GTProfesorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'idGrupoTrabajo' => 'required|exists:grupos_trabajo,id',
+            'idProfesor' => [
+                'required',
+                'exists:profesores,dni',
+                Rule::unique('miembros')->where(function ($query) {
+                    return $query
+                        ->where('idGrupoTrabajo', $this->input('idGrupoTrabajo'))
+                        ->where('idProfesor', $this->input('idProfesor'));
+                })
+            ]
+        ];
+    }
+}
