552ff0972e9dd8f0b45f4b506150dab4b9a2fa64
igomis <igomis@cipfpbatoi.es>
2023-09-26 18:17:59 +0200

    Fotos

--- resources/views/pdf/alumnos/partials/foto.blade.php
+++ resources/views/pdf/alumnos/partials/foto.blade.php
@@ -1,4 +1,4 @@
-<div style="float:left;width:23%;margin-bottom:5px;margin-right:10px;height:160px;{{ $style??'' }}"    >
+<div style="float:left;width:15%;margin-bottom:5px;margin-right:5px;height:140px;{{ $style??'' }}"    >
     <img src="{{$elemento->foto?public_path('/storage/'.$elemento->foto):''}}" style="width:125px;height:125px;margin-left:20;border:thin #000 solid;" /><br/>
     <h6 style="margin-top:-1px;clear:both;font-size:xx-small;">{{$elemento->nombre}} <b>{{  $elemento->posicion??'' }}</b> </h6>
 </div>
