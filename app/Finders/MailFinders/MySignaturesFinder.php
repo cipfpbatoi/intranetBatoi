d4d576806b3f2ef730df328b8603ac91e85f25a7
igomis <igomis@cipfpbatoi.es>
2024-02-09 10:25:52 +0100

    Enquestes

--- app/Finders/MailFinders/MySignaturesFinder.php
+++ app/Finders/MailFinders/MySignaturesFinder.php
@@ -1,6 +1,7 @@
 <?php
 namespace Intranet\Finders\MailFinders;
 
+use Illuminate\Support\Facades\DB;
 use Intranet\Entities\AlumnoFct;
 use Intranet\Http\Resources\SignaturaResource;
 
@@ -9,6 +10,7 @@ class MySignaturesFinder extends Finder
 {
     public function __construct()
     {
+
         $dni = apiAuthUser()->dni;
         $fcts = AlumnoFct::misFcts($dni)->whereNotNull('idSao')
             ->where('desde','>=', date('Y-m-d'))
@@ -17,6 +19,11 @@ class MySignaturesFinder extends Finder
                     ->orWhereHas('signatures', function ($query) {
                         $query->where('signed', 0);
                     });
+            })
+            ->whereNotIn('id', function($query) {
+                $query->select(DB::raw("SUBSTRING(route, POSITION('/' IN route) + 1)"))
+                    ->from('adjuntos')
+                    ->where('route', 'LIKE', 'alumnofctaval/%');
             })->get();
         $this->elements = SignaturaResource::collection($fcts);
     }
--- app/Http/Controllers/SignaturaController.php
+++ app/Http/Controllers/SignaturaController.php
@@ -5,6 +5,7 @@ namespace Intranet\Http\Controllers;
 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use Intranet\Botones\BotonBasico;
 use Intranet\Componentes\MyMail;
