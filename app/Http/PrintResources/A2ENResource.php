df62c9f20c1ea3cb71f170cc8dbe2507eb86ed2b
igomis <igomis@cipfpbatoi.es>
2024-04-08 08:40:13 +0200

    ANEXOS DUAL

--- app/Http/PrintResources/A2ENResource.php
+++ app/Http/PrintResources/A2ENResource.php
@@ -58,9 +58,9 @@ class A2ENResource extends PrintResource
             'Text Box 7_2' => day(Hoy()),
             'Text Box 7_3' => month(Hoy()),
             'Text Box 7_4' => substr(year(Hoy()), 2, 2),
-            'Text Box 7_7' => $director->fullName,
+            'Text Box 7_5' => $director->fullName,
             'Text Box 7_6' => $tutor->fullName,
-            'Text Box 7_5' => $empresa->gerente,
+            'Text Box 7_7' => $empresa->gerente,
         ];
     }
 }
