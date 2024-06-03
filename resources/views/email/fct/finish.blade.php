20bf097bdcb48fbb20848e4cc56622dd45d8b766
igomis <igomis@cipfpbatoi.es>
2024-06-03 09:16:27 +0200

    Canvi signatura tutor

--- resources/views/email/fct/finish.blade.php
+++ resources/views/email/fct/finish.blade.php
@@ -1,18 +1,16 @@
 <p>Hola, [contacte],</p>
-<p>T'escric per adjuntar-te el document que conté la <strong>llista de contactes</strong> que hem mantingut durant el període de pràctiques dels alumnes de FCT.</p>
-<p>Et demano que, si us plau, em facis arribar aquest document signat, amb la teva conformitat. Pots fer-ho digitalment si ho prefereixes.</p>
-<p>També t'agrairia que em proporcionares una valoració de les pràctiques per poder incloure-la en la secció corresponent del document d'avaluació dels alumnes.</p>
+<p>T'escric per <strong>adjuntar-te</strong> el document que conté la <strong>llista de contactes</strong> que hem mantingut durant el període de pràctiques de FCT.Et demane que em faces arribar aquest document signat, amb la teva conformitat. Pots fer-ho digitalment si ho prefereixes.</p>
+<p>També t'agrairia que em proporcionares una <strong>valoració de les pràctiques</strong> per poder incloure-la en la secció corresponent del <strong>document d'avaluació</strong> de l'alumnat.</p>
 <p>
-    En finalitzar l'avaluació dels alumnes, rebràs el certificat de les pràctiques juntament amb un correu electrònic amb un enllaç a una enquesta on podràs valorar com han anat les pràctiques. Aquesta enquesta ens ajuda a millorar la nostra tasca any rere any.
+    En finalitzar l'avaluació de l'alumnat, rebràs el certificat de les pràctiques juntament amb un correu electrònic amb un enllaç a una enquesta del departament de qualitat. Aquesta enquesta ens ajuda a millorar la nostra tasca any rere any.
 </p>
 <p>Moltes gràcies per la teva col·laboració. Rep una cordial salutació.</p>
 <hr>
 <p>Hola, [contacte],</p>
-<p>Te escribo para adjuntarte el documento que contiene la <strong>lista de contactos</strong> que hemos mantenido durante el periodo de prácticas de los alumnos de FCT.</p>
-<p>Te pido que, por favor, me hagas llegar este documento firmado, dando tu conformidad. Puedes hacerlo digitalmente si lo prefieres.</p>
-<p>También te agradecería que me proporcionases una valoración de las prácticas para poder incluirla en la sección correspondiente del documento de evaluación de los alumnos.</p>
+<p>Te escribo para <strong>adjuntarte</strong> el documento que contiene la <strong>lista de contactos</strong> que hemos mantenido durante el periodo de prácticas de FCT. Te pido que, por favor, me hagas llegar este documento firmado, dando tu conformidad. Puedes hacerlo digitalmente si lo prefieres.</p>
+<p>También te agradecería que me proporcionases una <strong>valoración de las prácticas</strong> para poder incluirla en la sección correspondiente del <strong>documento de evaluación</strong> del alumnado.</p>
 <p>
-    Al finalizar la evaluación de los alumnos, recibirás el certificado de las prácticas junto con un correo electrónico con un enlace a una encuesta donde podrás valorar cómo han ido las prácticas. Esta encuesta nos ayuda a mejorar nuestro trabajo año tras año.
+    Al finalizar la evaluación de los alumnos, recibirás el certificado de las prácticas junto con un correo electrónico con un enlace a una encuesta del dapartamento de calidad. Esta encuesta nos ayuda a mejorar nuestro trabajo año tras año.
 </p>
 <p>Muchas gracias por tu colaboración. Recibe un cordial saludo.</p>
 [peu]
