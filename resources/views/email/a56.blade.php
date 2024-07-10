0f997efa589903c84354fa1c3b9c978086086e90
igomis <igomis@cipfpbatoi.es>
2024-07-10 13:42:32 +0200

    Matricula

--- resources/views/email/a56.blade.php
+++ resources/views/email/a56.blade.php
@@ -13,7 +13,7 @@
     <div>
             <p>Hola, {{$remitente['tutor']}}</p>
             <p>Ha fallat la pujada dels arxius A5 a l'expedient de l'alumne perquè no hi ha 1 fitxers: {{ $elemento->Alumno->fullName }} del la fct {{ $elemento->id }}</p>
-            <p>Hi han {{ count($document) }} Documents. Si en tens més els has de juntar an un sol pdf.</p>
+            <p>Hi han {{ isset($document)?count($document):0 }} Documents. Si en tens més els has de juntar an un sol pdf.</p>
             <p>Si us plau, contacteu amb l'administrador del sistema per solucionar el problema.</p>
     </div>
 @endsection
