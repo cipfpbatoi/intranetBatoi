38536caef1f58fbf568a064418c72e84f24edc4c
igomis <igomis@cipfpbatoi.es>
2024-09-09 10:01:33 +0200

    Projecte

--- app/Http/Controllers/AdministracionController.php
+++ app/Http/Controllers/AdministracionController.php
@@ -117,7 +117,8 @@ class AdministracionController extends Controller
     private function ferVotsPermanents()
     {
         foreach (Vote::all() as $vote) {
-            if ($vote->Poll->Plantilla->remains && $fct = Fct::find($vote->idOption1)) {
+            //&& $fct = Fct::find($vote->idOption1)
+            if ($vote->Poll->Plantilla->remains ) {
                 dd($vote,$vote->Poll,$vote->Poll->Plantilla);
 
                 $newVote = new VoteAnt([
