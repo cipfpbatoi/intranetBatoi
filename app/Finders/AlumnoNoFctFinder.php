51caace768a65289c2c89145b2c65f688d27067f
igomis <igomis@cipfpbatoi.es>
2024-10-10 14:27:20 +0200

    Certificats que fallen

--- app/Finders/AlumnoNoFctFinder.php
+++ app/Finders/AlumnoNoFctFinder.php
@@ -10,7 +10,7 @@ class AlumnoNoFctFinder extends Finder
 
     public function exec(){
         // Obtenir les FCTs vinculades a l'alumne, incloure el nom de l'alumne i ordenar per aquest nom
-        $fcts = AlumnoFct::select('alumno_fcts.*', 'alumnos.nombre','alumnos.apellido1') // Assumeixo que 'nombre' és el camp del nom a la taula 'alumnos'
+        $fcts = AlumnoFct::select('alumno_fcts.*', 'alumnos.nombre','alumnos.apellido1' ) // Assumeixo que 'nombre' és el camp del nom a la taula 'alumnos'
         ->join('alumnos', 'alumno_fcts.idAlumno', '=', 'alumnos.nia') // Uneix amb la taula 'alumnos' per a obtenir el nom
         ->where('alumno_fcts.hasta', '>', date('Y-m-d')) // Filtrar les FCTs que no han finalitzat (la data de finalització és superior a la data actual
         ->misFcts($this->dni) // Aquesta crida sembla ser específica del model i s'utilitza per a filtrar les FCTs
@@ -23,28 +23,12 @@ class AlumnoNoFctFinder extends Finder
 
     private function filter(&$elements)
     {
-        //$uniqueAlumnos = [];
-        $filteredElements = [];
-
         foreach ($elements as $element) {
-            // Verificar si l'alumne ja ha estat afegit
-            /*if (!isset($uniqueAlumnos[$element->idAlumno])) {
-                // Afegir l'alumne a la llista de únics
-                $uniqueAlumnos[$element->idAlumno] = true;
-                $filteredElements[] = $element;
-            }*/
-            if (!isset($uniqueAlumnos[$element->idAlumno])) {
-                // Afegir l'alumne a la llista de únics
-                $uniqueAlumnos[$element->idAlumno] = true;
-
-            }
-
-            $filteredElements[] = $element;
-            // Marcar l'element segons la condició
-            $element->marked = ($element->pg0301 == 0);
+             $element->marked = ($element->pg0301 == 0);;
         }
+        return $elements;
 
-        return $filteredElements;
+
     }
 
 
