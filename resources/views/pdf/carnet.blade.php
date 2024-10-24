e4517954454f4c1fc91caa1ede76338123292591
Alexandre Coloma <alexandre.coloma+edu@gmail.com>
2024-10-24 16:14:53 +0200

    [ADD] padding carne lofo

--- resources/views/pdf/carnet.blade.php
+++ resources/views/pdf/carnet.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.pdf')
 @section('content')
     @foreach ($todos as $elemento)
-    <div class="page" style="border: orange 2px solid; padding: 1px" >
+    <div class="page">
         <div class="container col-lg-12" style="margin-bottom: 0px;" >
             <div style="float:left;width:26%;margin-left:15px"><img src="{{public_path('/img/pdf/logo.png')}}" width="60px" height="60px" alt="Logo Insti"/></div>
             <div style="float:left;width:26%;margin-left:15px"><img style="margin: auto" src="{{public_path('img/pdf/conselleria.png')}} " width="120px" height="60px" alt="Logo Conselleria"/></div>
             <div style="float:left;width:26%;margin-left:50px"><img style="float:right" src="{{public_path('img/pdf/ue.png')}}" width="60px" height="60px" alt="Logo UE"/></div>
         </div>
-        <div class="container col-lg-12 fondo"  style="background-image: url({{public_path('/img/pdf/insti.jpg')}});
+        <div class="container col-lg-12 fondo"  style="border: orange 2px solid; background-image: url({{public_path('/img/pdf/insti.jpg')}});
         background-repeat: no-repeat;
         background-size: 120% auto;
         overflow: hidden;
