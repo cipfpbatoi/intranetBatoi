72ab187b8a29723a15ad1b2b48cb7f5c05b9a08b
Alexandre Coloma <alexandre.coloma+edu@gmail.com>
2024-10-24 17:02:10 +0200

    [ADD] padding carne logo

--- resources/views/pdf/carnet.blade.php
+++ resources/views/pdf/carnet.blade.php
@@ -4,7 +4,7 @@
     <div class="page">
         <div class="container col-lg-12" style="margin-bottom: 0px;" >
             <div style="float:left;width:26%;margin-left:15px"><img src="{{public_path('/img/pdf/logo.png')}}" width="60px" height="60px" alt="Logo Insti"/></div>
-            <div style="float:left;width:26%;margin-left:15px"><img style="margin: auto" src="{{public_path('img/pdf/conselleria.png')}} " width="100px" height="50px" alt="Logo Conselleria"/></div>
+            <div style="float:left;width:26%;margin-left:15px"><img style="margin: auto" src="{{public_path('img/pdf/conselleria.png')}} " width="110px" height="55px" alt="Logo Conselleria"/></div>
             <div style="float:left;width:26%;margin-left:50px"><img style="float:right" src="{{public_path('img/pdf/ue.png')}}" width="60px" height="60px" alt="Logo UE"/></div>
         </div>
         <div class="container col-lg-12 fondo"  style="border: orange 1px solid; background-color: orange; background-image: url({{public_path('/img/pdf/insti.jpg')}});
