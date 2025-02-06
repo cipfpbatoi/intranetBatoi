463823419817d5ffcf6237dacd5ab7c86b3c9050
igomis <igomis@cipfpbatoi.es>
2025-02-06 13:37:24 +0100

    Trait Crud

--- resources/views/components/boton.blade.php
+++
@@ -1,9 +0,0 @@
-<a href="{{ $href }}" class="{{ $class }}" id="{{ $id }}"
-    @if($onclick) onclick="{{ $onclick }}" @endif>
-    @if($img)
-        <img src="{{ asset($img) }}" alt="Botó">
-    @elseif($icon)
-        <i class="{{ $icon }}"></i>
-    @endif
-    {{ $text }}
-</a>
