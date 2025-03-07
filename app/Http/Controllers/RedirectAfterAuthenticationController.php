a71d6432e5bec28ec20ac5200298c03334a9491a
igomis <igomis@cipfpbatoi.es>
2025-03-07 11:55:45 +0100

    Redirect

--- app/Http/Controllers/RedirectAfterAuthenticationController.php
+++ app/Http/Controllers/RedirectAfterAuthenticationController.php
@@ -5,47 +5,51 @@ namespace Intranet\Http\Controllers;
 use Intranet\Http\Requests\PasswordRequest;
 use Intranet\Services\SeleniumService;
 use Styde\Html\Facades\Alert;
-
+use Illuminate\Support\Str;
+use Throwable;
+use ReflectionMethod;
+use Exception;
 
 class RedirectAfterAuthenticationController extends Controller
 {
-
-
     public function __invoke(PasswordRequest $request)
     {
-        $className = 'Intranet\Sao\\' . ucfirst($request->accion);
+        $className = $this->resolveClassName($request->accion);
 
         if (!class_exists($className)) {
-            throw new \Exception("La classe $className no existeix.");
+            throw new Exception("La classe $className no existeix.");
         }
 
-        $caps = method_exists($className, 'setFireFoxCapabilities') ? $className::setFireFoxCapabilities() : null;
+        $caps = method_exists($className, 'setFireFoxCapabilities')
+            ? $className::setFireFoxCapabilities()
+            : null;
 
         try {
             $driver = SeleniumService::loginSAO(authUser()->dni, $request->password, $caps);
-
-            $reflection = new \ReflectionMethod($className, 'index');
-            // Si la classe té un mètode estàtic `handle`, el cridem directament
-            if ($reflection->isStatic()) {
-                if ($request->hasFile('file')) {
-                    return $className::index($driver, $request->toArray(), $request->file('file'));
-                }
-                return $className::index($driver, $request->toArray());
-            }
-
-            // Si no té `handle` estàtic, la instanciem i cridem el mètode corresponent
-            $classInstance = app($className);
-            if ($request->hasFile('file')) {
-                return $classInstance->index($driver, $request->toArray(), $request->file('file'));
-            }
-            return $classInstance->index($driver, $request->toArray());
-
-        } catch (\Throwable $exception) {
+            return $this->executeAction($className, $driver, $request);
+        } catch (Throwable $exception) {
             Alert::info($exception->getMessage());
-            if (isset($driver)) {
-                $driver->close();
-            }
+            $driver?->close();
             return back();
         }
+    }
+
+    private function resolveClassName(string $action): string
+    {
+        return 'Intranet\\Sao\\' . Str::ucfirst($action);
+    }
+
+    private function executeAction(string $className, $driver, PasswordRequest $request)
+    {
+        $reflection = new ReflectionMethod($className, 'index');
+        $parameters = [$driver, $request->toArray()];
+
+        if ($request->hasFile('file')) {
+            $parameters[] = $request->file('file');
+        }
+
+        return $reflection->isStatic()
+            ? $className::index(...$parameters)
+            : app($className)->index(...$parameters);
     }
 }
