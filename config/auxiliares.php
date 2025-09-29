467e65bffa9b8f9cab63ea0d92c3b1860fd59c5b
Alexandre Coloma <alexandre.coloma+edu@gmail.com>
2025-09-29 20:29:25 +0200

    [ADD] motiu falta

--- config/auxiliares.php
+++ config/auxiliares.php
@@ -52,7 +52,9 @@ return [
         'PRE.CANDI.ELECCIONES',
         'TRASLADO DE DOMICILIO HABITUAL',
         'TÉCNICAS PRENATALES',
-        'ASSISTÈNCIA TRIBUNALS (Màx 2. DIES /SETMANA)'],
+        'ASSISTÈNCIA TRIBUNALS (Màx 2. DIES /SETMANA)',
+        'FALTA EMERGÈNCIA CLIMÂTICA (Màx 4. DIES / ANY)'
+        ],
     'motivoAusenciaOld' => [
         'Baixa mèdica',
         'LLicència per formació',
