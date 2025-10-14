b156cfa5d3a208092f639d7f9c2fa09bdeafe113
Ignasi <igomis@cipfpbatoi.es>
2025-10-14 12:02:44 +0200

    ACTAfse

--- resources/views/pdf/reunion/actaFSE.blade.php
+++ resources/views/pdf/reunion/actaFSE.blade.php
@@ -3,12 +3,17 @@
     $grupo = $datosInforme;
     $todos = $grupo->Alumnos;
 @endphp
-@section('content')
+@if ($grupo)
+    @section('content')
         @include('pdf.partials.cabeceraFSE')
         @if ($grupo->Ciclo->tipo < 3)
             @include('pdf.reunion.partials.MiS_FSE')
         @else
             @include('pdf.reunion.partials.BAS_FSE')
        @endif
-@endsection
-
+    @endsection
+@else
+    @section('content')
+        <p>Esta acta s'ha d'imprimir des de les opcions del menú i després s'ha de crear l'acta corresponent i adjuntar l'arxiu imprés. També s'ha de baixar una còpia a Caporalia signada pel tutor.</p>
+    @endsection
+@endif
