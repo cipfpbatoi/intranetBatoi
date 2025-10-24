a6d215aecc5b68e54bfc6c26c3c30ee9b6528097
Ignasi <igomis@cipfpbatoi.es>
2025-10-24 10:45:06 +0200

    Rolemiddleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -3,12 +3,18 @@
 namespace Intranet\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class RoleMiddleware
 {
 
     public function handle($request, Closure $next, $role)
     {
+        $user = Auth::user();
+        if (!$user) {
+            // No autenticat -> al login amb missatge
+            return redirect()->route('login');
+        }
         if (userIsNameAllow($role) || isAdmin()) {
             return $next($request);
         }
