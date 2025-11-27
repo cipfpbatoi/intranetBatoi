80d4466caaca39e637bf8058edf30dd1e1b6165c
igomis <igomis@cipfpbatoi.es>
2025-11-27 09:34:22 +0100

    Cerca en standard

--- resources/views/intranet/partials/grid/standard.blade.php
+++ resources/views/intranet/partials/grid/standard.blade.php
@@ -1,8 +1,26 @@
 <div class="x_content">
+    @if($panel->getPaginator())
+        <div class="d-flex justify-content-end mb-2">
+            <form method="get" class="w-100" style="max-width: 480px;">
+                <div class="input-group input-group-sm">
+                    <input type="text"
+                           name="search"
+                           class="form-control"
+                           value="{{ request('search') }}"
+                           placeholder="Cercar document...">
+                    <button class="btn btn-primary" type="submit">{{ __('Cercar') }}</button>
+                    @if(request('search'))
+                        <a class="btn btn-outline-secondary" href="{{ url()->current() }}">{{ __('Netejar') }}</a>
+                    @endif
+                </div>
+            </form>
+        </div>
+    @endif
     <table id='datatable'
            name='{{$panel->getModel()}}'
            class="table table-striped table-bordered display nowrap dtr-inline collapsed"
-           style='width:100%' data-page-length="25">
+           style='width:100%' data-page-length="25"
+           data-server-pagination="{{ $panel->getPaginator() ? 'true' : 'false' }}">
         <thead>
             @include('intranet.partials.grid.campos')
         </thead>
