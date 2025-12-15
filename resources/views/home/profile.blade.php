ca903fba99cfba95ef30543a741e73098583be8a
Ignasi <igomis@cipfpbatoi.es>
2025-12-15 13:18:10 +0100

    llevar usuari

--- resources/views/home/profile.blade.php
+++ resources/views/home/profile.blade.php
@@ -1,5 +1,4 @@
 <x-layouts.app title="Panell de Control">
-    <x-user-profile :usuario="$usuario" />
     <div class="col-md-9 col-sm-9 col-xs-12">
         <x-user-tabs :tabs="[
             ['title' => 'messages.generic.nextActivities', 'view' => 'home.partials.activities', 'data' => ['actividades' => $actividades]],
