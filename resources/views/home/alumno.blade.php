6fbba2a02a24ee54a14c95df1f2302aea03dfec3
igomis <igomis@cipfpbatoi.es>
2025-12-21 08:58:31 +0100

    Vista principal de edición de mensajes de confirmación de Git

--- resources/views/home/alumno.blade.php
+++ resources/views/home/alumno.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.app  title="Panell de Control">
-    <div class="col-md-9 col-sm-9 col-xs-12">
+    <div class="col-md-11 col-sm-11 col-xs-12">
         <x-user-tabs :tabs="[
             ['title' => 'messages.generic.nextActivities', 'view' => 'home.partials.activities', 'data' => ['actividades' => $actividades]],
             ['title' => 'messages.generic.timeTable', 'view' => 'home.partials.horario.grupo', 'data' => ['horario' => $horario]],
