db1d4d8217b58a6b7e7eb73b594a93b83b771f59
igomis <igomis@cipfpbatoi.es>
2025-12-30 18:14:25 +0100

    fiel-input

--- resources/views/components/form/file-input.blade.php
+++ resources/views/components/form/file-input.blade.php
@@ -15,26 +15,18 @@
     $fieldId = $attributes['id'] ?? $name;
 @endphp
 
-<div id="field_{{ $fieldId }}" class="form-group item">
-    <label for="{{ $fieldId }}" class="control-label col-md-3 col-sm-3 col-xs-12" style="margin-left: 5px;">
-        {{ $label ?? __('validation.attributes.' . $name) }}
-        @if ($isRequired)
-            <span class="required">*</span>
-        @endif
-    </label>
-    <div class="col-md-6 col-sm-6 col-xs-12">
-        @if ($currentFile)
-            <p class="form-control-static">
-                <a href="{{ asset('storage/' . $currentFile) }}" target="_blank" rel="noopener">
-                    {{ basename($currentFile) }}
-                </a>
-            </p>
-        @endif
+{!! Field::file($name, $attributes) !!}
 
-        {!! Field::file($name, $attributes) !!}
-
-        @if ($isDisabled)
-            {!! Field::hidden($name, null, []) !!}
-        @endif
+@if ($currentFile)
+    <div class="col-md-offset-3 col-md-6 col-sm-6 col-xs-12" style="margin-bottom: 10px;">
+        <p class="form-control-static">
+            <a href="{{ asset('storage/' . $currentFile) }}" target="_blank" rel="noopener">
+                {{ basename($currentFile) }}
+            </a>
+        </p>
     </div>
-</div>
+@endif
+
+@if ($isDisabled)
+    {!! Field::hidden($name, null, []) !!}
+@endif
