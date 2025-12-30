c68d5e19b67ad5d86376961ea0558b0ede85c14f
igomis <igomis@cipfpbatoi.es>
2025-12-30 18:33:40 +0100

    days

--- resources/views/fct/days.blade.php
+++ resources/views/fct/days.blade.php
@@ -1,6 +1,11 @@
-@php($title = 'Calendari FCT de '.$alumnoFct->fullName)
+@php
+    // Acceptem tant $alumno com $alumnoFct per compatibilitat amb diferents controladors
+    $student = $alumno ?? optional($alumnoFct)->Alumno;
+    $title = $student ? 'Calendari FCT de ' . $student->fullName : 'Calendari FCT';
+@endphp
+
 <x-pages.livewire
-        :title="$title"
-         component="fct-calendar"
-        :params="['alumnoFct' => $alumnoFct]"
+    :title="$title"
+    component="fct-calendar"
+    :params="['alumnoFct' => $student]"
 />
