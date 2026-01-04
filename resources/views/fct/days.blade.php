614e56ec7680db5e2a92ed8e35401fd42f29b5f6
igomis <igomis@cipfpbatoi.es>
2026-01-04 13:16:23 +0100

    days

--- resources/views/fct/days.blade.php
+++ resources/views/fct/days.blade.php
@@ -1,11 +1,14 @@
 @php
     // Acceptem tant $alumno com $alumnoFct per compatibilitat amb diferents controladors
+    $alumnoFct = $alumnoFct ?? null;
     $student = $alumno ?? optional($alumnoFct)->Alumno;
+    $studentId = optional($student)->id;
+    $alumnoFctId = optional($alumnoFct)->id;
     $title = $student ? 'Calendari FCT de ' . $student->fullName : 'Calendari FCT';
 @endphp
 
 <x-pages.livewire
     :title="$title"
     component="fct-calendar"
-    :params="['alumnoFct' => $student]"
+    :params="['alumno' => $studentId, 'alumnoFct' => $alumnoFctId]"
 />
