f50c89e049340aae0d404bb1567bbad14b073e19
igomis <igomis@cipfpbatoi.es>
2026-01-30 13:36:03 +0100

    Log en SeleniumService

--- app/Services/SeleniumService.php
+++ app/Services/SeleniumService.php
@@ -13,6 +13,7 @@ use Intranet\Exceptions\IntranetException;
 use Intranet\Exceptions\SeleniumException;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Illuminate\Support\Facades\Log;
 
 class SeleniumService
 {
