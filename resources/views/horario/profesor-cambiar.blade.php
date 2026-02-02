caa053caafc19103382b9ff32ee1f2eb4326c9a3
igomis <igomis@cipfpbatoi.es>
2026-02-02 13:28:17 +0100

    Horari Cavniar

--- resources/views/horario/profesor-cambiar.blade.php
+++ resources/views/horario/profesor-cambiar.blade.php
@@ -1,24 +1,5 @@
-@extends('layouts.intranet')
-@section('content')
-	<p>Propuesta de modificación del horario que deberá ser aprobada por jefatura de estudios.</p>
-	<p>Sobre tu horario original cambia las horas arrástrandolas a donde desees
-		y cuando lo tengas pulsa el botón de 'Guardar cambios'.</p>
-	<p>Cuando lo haya aprobado jefatura te aparecerá tu horario cambiado.</p>
-	<p>En el campo observaciones debes poner el nº de horas que liberas y
-		a qué tareas dedicarás cada una de dichas horas.</p>
-	<strong>Estado: </strong><input type="text" id="estado" readonly><br>
-	<button id="init">Iniciar Procedimiento</button>
-	@include('home.partials.horario.profesor')
-	Observaciones:
-	<textarea class="form-control" id="obs" placeholder="Indica el nº de horas liberadas y a qué las dedicarás"></textarea>
-	<br>
-	<button id="guardar">Guardar cambios y enviar</button>
-	<button id="aplicar" hidden="on">Aplicar cambio horario</button>
-@endsection
-@section('titulo')
-Horario {{$profesor->fullName}}
-@endsection
-@section('scripts')
-{{ Html::script("/js/Horario/cambiar.js") }}
-@endsection
-
+<x-pages.livewire
+    title="Horari {{ $profesor->fullName }}"
+    component="horari-professor-canvi"
+    :params="['dni' => $profesor->dni]"
+/>
--- routes/profesor.php
+++ routes/profesor.php
@@ -160,6 +160,7 @@ Route::get('/profesor/{profesor}/tarjeta', ['as' => 'profesor.tarjeta', 'uses' =
 Route::post('/profesor/{profesor}/mensaje', ['as' => 'profesor.mensaje', 'uses' => 'ProfesorController@alerta']);
 Route::post('/profesor/colectivo', ['as' => 'profesor.colectivo', 'uses' => 'ProfesorController@avisaColectivo']);
 Route::get('/horario/change', ['as' => 'horario.change', 'uses' => 'HorarioController@horarioCambiar']);
+Route::get('/horario/canvi-horari-temporal', ['as' => 'horario.canvi.temporal', 'uses' => 'HorarioController@horarioCambiar']);
 Route::get('/profesor/{profesor}/horario-aceptar', ['as' => 'horario.aceptar', 'uses' => 'HorarioController@changeTable']);
 Route::get('/profesor/{profesor}/horario-cambiar', ['as' => 'horario.profesor.change', 'uses' => 'HorarioController@horarioCambiar']);
 
