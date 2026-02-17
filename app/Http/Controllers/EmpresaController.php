4ae99a0f4dbe1aef1fee100ee615cf5cc7b778c6
igomis <igomis@cipfpbatoi.es>
2026-02-17 16:39:37 +0100

    Refactor Empresa with repository and application service

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -2,16 +2,17 @@
 
 namespace Intranet\Http\Controllers;
 
+use Intranet\Application\Empresa\EmpresaService;
 use Intranet\Application\Grupo\GrupoService;
 use Intranet\Http\Controllers\Core\IntranetController;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Intranet\Entities\Empresa;
 use Intranet\Entities\Centro;
 use Intranet\Entities\Colaboracion;
-use Intranet\Entities\Ciclo;
+use Intranet\Http\Requests\EmpresaRequest;
 use Intranet\Http\PrintResources\A1Resource;
+use Intranet\Presentation\Crud\EmpresaCrudSchema;
 use Intranet\Services\Document\FDFPrepareService;
 use Intranet\UI\Botones\BotonBasico;
 use Styde\Html\Facades\Alert;
@@ -19,16 +20,19 @@ use Styde\Html\Facades\Alert;
 class EmpresaController extends IntranetController
 {
     private ?GrupoService $grupoService = null;
+    private ?EmpresaService $empresaService = null;
 
     protected $perfil = 'profesor';
     protected $model = 'Empresa';
-    protected $gridFields = ['concierto', 'nombre', 'direccion', 'localidad', 'telefono', 'email', 'cif', 'actividad'];
+    protected $gridFields = EmpresaCrudSchema::GRID_FIELDS;
     protected $vista = ['show' => 'empresa','index'=>'vacia'];
 
-    public function __construct(?GrupoService $grupoService = null)
+    public function __construct(?GrupoService $grupoService = null, ?EmpresaService $empresaService = null)
     {
         parent::__construct();
         $this->grupoService = $grupoService;
+        $this->empresaService = $empresaService;
+        $this->formFields = EmpresaCrudSchema::FORM_FIELDS;
     }
 
     private function grupos(): GrupoService
@@ -40,12 +44,18 @@ class EmpresaController extends IntranetController
         return $this->grupoService;
     }
 
+    private function empreses(): EmpresaService
+    {
+        if ($this->empresaService === null) {
+            $this->empresaService = app(EmpresaService::class);
+        }
+
+        return $this->empresaService;
+    }
+
     protected function search()
     {
-        return Empresa::query()
-            ->select(['id', 'concierto', 'nombre', 'direccion', 'localidad', 'telefono', 'email', 'cif', 'actividad'])
-            ->orderBy('nombre')
-            ->get();
+        return $this->empreses()->listForGrid();
     }
 
     
@@ -57,24 +67,12 @@ class EmpresaController extends IntranetController
     public function show($id)
     {
         $activa = Session::get('pestana') ? Session::get('pestana') : 2;
-        $elemento = Empresa::with([
-            'centros.colaboraciones.ciclo',
-            'centros.instructores',
-        ])->findOrFail($id);
+        $elemento = $this->empreses()->findForShow((int) $id);
         $modelo = 'Empresa';
 
         $cicloTutoria = $this->grupos()->find((string) AuthUser()->GrupoTutoria)?->idCiclo;
-        $misColaboracionesIds = collect();
-        if ($cicloTutoria) {
-            $misColaboracionesIds = Colaboracion::query()
-                ->where('idCiclo', $cicloTutoria)
-                ->whereIn('idCentro', $elemento->centros->pluck('id'))
-                ->pluck('id');
-        }
-
-        $ciclosDepartamento = Ciclo::query()
-            ->where('departamento', authUser()->departamento)
-            ->get();
+        $misColaboracionesIds = $this->empreses()->colaboracionIdsForTutorCycle($cicloTutoria, $elemento);
+        $ciclosDepartamento = $this->empreses()->departmentCycles((string) authUser()->departamento);
         $ciclosDepartamentoIds = $ciclosDepartamento->pluck('id')->all();
 
         return view('empresa.show', compact(
@@ -93,14 +91,9 @@ class EmpresaController extends IntranetController
         $this->panel->setBoton('index', new BotonBasico("empresa.create", ['roles' => config('roles.rol.tutor')]));
      }
 
-    public function store(Request $request)
+    public function store(EmpresaRequest $request)
     {
-        $cif = strtoupper((string) $request->cif);
-        if (Empresa::where('cif', $cif)->exists()) {
-            return back()->withInput($request->all())->withErrors('CIF duplicado');
-        }
-
-        $id = $this->realStore(subsRequest($request, ['cif' => $cif]));
+        $id = $this->realStore($request);
 
         $idCentro = $this->createCenter($id, $request);
         $idCicloTutoria = $this->grupos()->firstByTutor(AuthUser()->dni)?->idCiclo;
@@ -113,7 +106,7 @@ class EmpresaController extends IntranetController
 
 
 
-    private function createCenter($id, Request $request)
+    private function createCenter($id, EmpresaRequest $request)
     {
         $centro = new Centro();
         $centro->idEmpresa = $id;
@@ -123,7 +116,7 @@ class EmpresaController extends IntranetController
         $centro->save();
         return $centro->id;
     }
-    private function createColaboration($id, Request $request, $idCicloTutoria)
+    private function createColaboration($id, EmpresaRequest $request, $idCicloTutoria)
     {
         $colaboracion = new Colaboracion();
         $colaboracion->idCentro = $id;
@@ -136,20 +129,15 @@ class EmpresaController extends IntranetController
         return $colaboracion->id;
     }
     
-    protected function realStore(Request $request, $id = null)
+    protected function realStore(EmpresaRequest $request, $id = null)
     {
         $elemento = $id ? Empresa::findOrFail($id) : new Empresa(); //busca si hi ha
-        if ($id) {
-            $elemento->setRule('concierto', $elemento->getRule('concierto').','.$id);
-        }
-        $this->validateAll($request, $elemento);    // valida les dades
-
         return $elemento->fillAll($request);        // ompli i guarda
     }
     
-    public function update(Request $request, $id)
+    public function update(EmpresaRequest $request, $id)
     {
-        $elemento = Empresa::find($this->realStore(subsRequest($request, ['cif'=>strtoupper($request->cif)]), $id));
+        $elemento = Empresa::find($this->realStore($request, $id));
 
         foreach ($elemento->centros as $centro) {
             $touched = false;
