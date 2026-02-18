fbfdfccebbf62e4e030751938e95c30b8a03bc28
igomis <igomis@cipfpbatoi.es>
2026-02-18 22:28:34 +0100

    FaltaCrudSchema

--- app/Http/Controllers/FaltaController.php
+++ app/Http/Controllers/FaltaController.php
@@ -76,6 +76,8 @@ class FaltaController extends IntranetController
      */
     public function store(Request $request)
     {
+        $this->validateAll($request, new Falta());
+
         $id = $this->faltas()->create($request);
 
         if (!$request->boolean('baja') && UserisAllow(config('roles.rol.direccion'))) {
@@ -96,6 +98,8 @@ class FaltaController extends IntranetController
      */
     public function update(Request $request, $id)
     {
+        $this->validateAll($request, Falta::findOrFail($id));
+
         $this->faltas()->update($id, $request);
         return $this->redirect();
     }
