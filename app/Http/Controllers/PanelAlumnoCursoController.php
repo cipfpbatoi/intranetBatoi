f360873fb1736027efc16bce66d6c4f341a96294
Ignasi <igomis@cipfpbatoi.es>
2026-02-27 12:10:18 +0100

    PanelAlumnoCursoController

--- app/Http/Controllers/PanelAlumnoCursoController.php
+++ app/Http/Controllers/PanelAlumnoCursoController.php
@@ -4,8 +4,6 @@ namespace Intranet\Http\Controllers;
 
 use Intranet\Http\Controllers\Core\BaseController;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Gate;
 use Intranet\Entities\Curso;
 
 
@@ -31,7 +29,7 @@ class PanelAlumnoCursoController extends BaseController
      */
     public function index()
     {
-        Gate::authorize('viewAny', Curso::class);
+        $this->authorizeForUser(authUser(), 'viewAny', Curso::class);
         return parent::index();
     }
 
@@ -39,7 +37,7 @@ class PanelAlumnoCursoController extends BaseController
      * @return mixed
      */
     public function search(){
-        Gate::authorize('viewAny', Curso::class);
+        $this->authorizeForUser(authUser(), 'viewAny', Curso::class);
         return Curso::where('activo', 1) ->get();
     }
 
@@ -48,7 +46,7 @@ class PanelAlumnoCursoController extends BaseController
      */
     protected function iniBotones()
     {
-        Gate::authorize('viewAny', Curso::class);
+        $this->authorizeForUser(authUser(), 'viewAny', Curso::class);
         $this->panel->setBothBoton('alumnocurso.register', ['class' => 'btn-success authorize'], true);
         $this->panel->setBothBoton('alumnocurso.unregister', ['class' => 'btn-danger unauthorize'], true);
     }
