f1a5b7aca3f819292996f0579de7ca4c941967ba
igomis <igomis@cipfpbatoi.es>
2026-03-12 10:54:26 +0100

    [MOD] Iniciar Sprint 4 amb backlog i migració JS nadiu de col·laboració/empresa #78

--- public/js/Colaboracion/modal.js
+++ public/js/Colaboracion/modal.js
@@ -1,8 +1,24 @@
 'use strict';
-$(function () {
-    $('.fa-plus').on("click", function(){
-        var fct = this.id;
-        $('#formFct').attr('action', '/fct/'+fct+'/alumnoCreate');
-    });
-});
 
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
+        var formFct = document.getElementById('formFct');
+        if (!formFct) {
+            return;
+        }
+
+        document.addEventListener('click', function (event) {
+            var trigger = event.target.closest('.fa-plus');
+            if (!trigger) {
+                return;
+            }
+
+            var fct = trigger.id;
+            if (!fct) {
+                return;
+            }
+
+            formFct.setAttribute('action', '/fct/' + fct + '/alumnoCreate');
+        });
+    });
+})();
--- public/js/Empresa/delete.js
+++ public/js/Empresa/delete.js
@@ -1,25 +1,45 @@
-$(function() {
-	$(".fa-trash").on('click', function(event) {
-		let info=$(this).parents('li').find('.info').text();
-                if ($(this).parents('ul').hasClass('colaboracion')) 
-                    opcion = 'la colaboración entre';
-                else
-                    if ($(this).parents('ul').hasClass('fct'))
-                        opcion = 'la fct de ';
-                    else    
-                        if ($(this).parent('a').hasClass('instructor')){
-                            opcion = 'el instructor';
-                            info = $(this).parents('h4.text-info').find('.nombre').text();
-                        }
-                        else    
-                           opcion = 'el centro de trabajo';
-		if (!confirm('Vas a borrar '+opcion+': '+info)) {
-			event.preventDefault();
-		}
-	})
-        $("#Borrar").on('click', function(event) {
-		if (!confirm('!! Vas a borrar la empresa, los centros y todas sus colaboraciones !!')) {
-			event.preventDefault();
-		}
-	})
-})
+'use strict';
+
+(function () {
+    function getInfoAndOption(trigger) {
+        var option = 'el centro de trabajo';
+        var list = trigger.closest('ul');
+        var infoNode = trigger.closest('li') ? trigger.closest('li').querySelector('.info') : null;
+        var info = infoNode ? infoNode.textContent : '';
+
+        if (list && list.classList.contains('colaboracion')) {
+            option = 'la colaboración entre';
+        } else if (list && list.classList.contains('fct')) {
+            option = 'la fct de ';
+        } else {
+            var anchor = trigger.closest('a');
+            if (anchor && anchor.classList.contains('instructor')) {
+                option = 'el instructor';
+                var nombreNode = trigger.closest('h4.text-info') ? trigger.closest('h4.text-info').querySelector('.nombre') : null;
+                info = nombreNode ? nombreNode.textContent : info;
+            }
+        }
+
+        return { option: option, info: info };
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('click', function (event) {
+            var trashIcon = event.target.closest('.fa-trash');
+            if (trashIcon) {
+                var data = getInfoAndOption(trashIcon);
+                if (!window.confirm('Vas a borrar ' + data.option + ': ' + data.info)) {
+                    event.preventDefault();
+                }
+                return;
+            }
+
+            var deleteButton = event.target.closest('#Borrar');
+            if (deleteButton) {
+                if (!window.confirm('!! Vas a borrar la empresa, los centros y todas sus colaboraciones !!')) {
+                    event.preventDefault();
+                }
+            }
+        });
+    });
+})();
