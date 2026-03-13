d5e7adf1f3d23107655b2502e583ded5133bf721
Arturo Candela <a.candelamolto@edu.gva.es>
2026-03-13 11:54:55 +0100

    [ADD][FIX] #87 Filtrar enquestes per curs de l'alumne/professor
    - Afegeix columna `curs` (nullable tinyint) a la taula `polls` per indicar
      el curs destinatari (NULL = tots els cursos, 1 = 1r, 2 = 2n)
    - Actualitza Poll: `$fillable`, `$inputTypes` i `getCursOptions()` per al
      selector al formulari d'administració
    - Afegeix `getUserCurs()` a PollController (compartit per subcontroladors):
      obté el curs de l'alumne via Grupo o del professor via GrupoTutoria
    - PanelPollResponseController: filtra les enquestes actives per curs de l'usuari
    - PanelPollResultController: filtra els resultats per curs via whereNull/orWhere

--- app/Http/Controllers/PanelPollResultController.php
+++ app/Http/Controllers/PanelPollResultController.php
@@ -6,27 +6,43 @@ use Intranet\UI\Botones\BotonImg;
 use Intranet\Entities\Poll\Poll;
 use Intranet\Entities\Poll\PPoll;
 
+/**
+ * Controlador per mostrar els resultats d'enquestes finalitzades als professors.
+ * Filtra per tipus d'enquesta segons el rol i per curs del grup del professor.
+ */
 class PanelPollResultController extends PollController
 {
-    protected $gridFields = [ 'id','title'];
+    protected $gridFields = ['id', 'title'];
 
     protected function iniBotones()
     {
-        $this->panel->setBoton('grid',new BotonImg('poll.show',['img' =>'fa-eye']));
+        $this->panel->setBoton('grid', new BotonImg('poll.show', ['img' => 'fa-eye']));
     }
 
+    /**
+     * Retorna les enquestes finalitzades visibles per a l'usuari autenticat,
+     * filtrades per rol i per curs del seu grup.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     protected function search()
     {
         if (esRol(AuthUser()->rol, config('roles.rol.tutor'))) {
-            $ppolls = hazArray(PPoll::all(),'id','id');
+            $ppolls = hazArray(PPoll::all(), 'id', 'id');
+        } else {
+            $ppolls = hazArray(PPoll::where('what', 'Profesor')->get(), 'id', 'id');
         }
-        else {
-            $ppolls = hazArray(PPoll::where('what', 'Profesor')->get(),'id','id');
+
+        $query = Poll::whereIn('idPPoll', $ppolls)->where('hasta', '<=', now());
+
+        $userCurs = $this->getUserCurs();
+        
+        if ($userCurs !== null) {
+            $query->where(function ($q) use ($userCurs): void {
+                $q->whereNull('curs')->orWhere('curs', $userCurs);
+            });
         }
-        return Poll::whereIn('idPPoll', $ppolls)
-            ->where('hasta', '<=', now())
-            ->get();
+
+        return $query->get();
     }
-
-
 }
