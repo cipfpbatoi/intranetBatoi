e0f0dbc0f5a2dc6b4bcb7507525465a1267c76ef
igomis <igomis@cipfpbatoi.es>
2026-03-25 15:00:20 +0100

    [DEL] Retira la branca Vue de reservas #142

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,10 +2,6 @@
 import Vue from 'vue'
 window.Vue = Vue;
 
-import ReservasView from './components/reservas/ReservasView.vue';
-
-Vue.component('reservas-view', ReservasView);
-
 const app = new Vue({
     el: '#app'
 });
