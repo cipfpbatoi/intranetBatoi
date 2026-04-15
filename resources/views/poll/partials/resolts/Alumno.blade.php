0f565619983c2314b3d444408bf26e91f15eebab
Ignasi <igomis@cipfpbatoi.es>
2026-04-15 12:09:38 +0200

    [FIX] Permet que l'alumnat responga enquestes sense FCT #176

---
+++ resources/views/poll/partials/resolts/Alumno.blade.php
@@ -0,0 +1,18 @@
+@php($grupo = authUser()?->Grupo?->first())
+<h2>Enquesta alumnat</h2>
+<table style="border: #00aeef 1px solid">
+    <thead>
+    <tr>
+        <td>Enquesta</td>
+        @foreach ($options as $item) <th>{{$item->question}} </th> @endforeach
+    </tr>
+    </thead>
+    @foreach ($myVotes as $studentVotes)
+        <tr>
+            <td>{{ $grupo ? 'Grup '.$grupo->codigo : 'Resposta' }}</td>
+            @foreach ($options as $item)
+                <td>{{ $studentVotes[$item->id] ?? '' }}</td>
+            @endforeach
+        </tr>
+    @endforeach
+</table>
