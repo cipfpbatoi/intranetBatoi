b2063d3574b0c07b7ba9baadf9f42455f3d395cf
Alexandre Coloma <alexandre.coloma+edu@gmail.com>
2026-05-19 18:51:30 +0200

    [MOD] Afig baixa de professorat amb modal i data

---
+++ resources/views/intranet/partials/modal/baja.blade.php
@@ -0,0 +1,4 @@
+<x-modal name="baja" title="Baixa de professor" message="Confirmar baixa">
+    <label class="control-label" for="fecha_baja">Data de baixa:</label>
+    <input type="date" id="fecha_baja" name="fecha_baja" class="form-control" required>
+</x-modal>
