605fd8148e4e303b00b54b71b6aeda5e053b6528
Arturo Candela <frisix@gmail.com>
2026-06-02 13:34:32 +0200

    [FIX] Evita doble inicialització de DataTables a /tutoria i corregeix 'dsc'

--- public/js/Tutoria/index.js
+++ public/js/Tutoria/index.js
@@ -1,5 +1,15 @@
 'use strict';
-$(document).ready(function() {
-    var table = $('#datatable').DataTable();
-    table.order([2, 'dsc']).draw();
+$(document).ready(function () {
+    var $table = $('#datatable');
+    if (!$table.length) { return; }
+
+    function applyOrder() {
+        $table.DataTable({ retrieve: true }).order([2, 'desc']).draw();
+    }
+
+    if ($.fn.dataTable && $.fn.dataTable.isDataTable($table[0])) {
+        applyOrder();
+    } else {
+        $table.on('init.dt', applyOrder);
+    }
 });
